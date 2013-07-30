a7cacfddc259960857e16c02a77f801bc3219f00
daedalus <unknownunreleased@gmail.com>
2013-07-30 20:25:55 +0200

    simple message dispatching in ES

--- entity_system/processing_system.h
+++ entity_system/processing_system.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <cassert>
 #include "signature_matcher.h"
 
 namespace augmentations {
@@ -8,11 +9,11 @@ namespace augmentations {
 		class processing_system {
 			friend class world;
 			friend class entity;
-			std::vector<entity*> targets;
-			
+
 			signature_matcher_bitset components_signature;
 		public:
-			
+			std::vector<entity*> targets;
+		
 			/* add an entity to processing_system 
 			base function just push_backs entity to targets
 			*/
@@ -25,16 +26,23 @@ namespace augmentations {
 			virtual void remove(entity*);
 
 			/* process all entity targets */
-			virtual void process_entities() = 0;
+			virtual void process_entities(world&) = 0;
 
 			/* you are required to override this function to specify components that this system needs to processing */
 			virtual type_pack get_needed_components() const = 0;
 
-			/* "targets" read-only getter */
-			const std::vector<entity*>& get_targets() const;
-
 			/* helper function that iterates through every target entity */
 			void for_each(std::function<void (entity*)>);
+
+			template<typename message>
+			void post(world& owner, const message& msg) const {
+				owner.post_message(msg);
+			}
+
+			template<typename message>
+			std::vector<message>& get_queue(world& owner, const message& msg) const {
+				owner.get_message_queue<message>();
+			}
 		};
 
 		/* helper class removing necessity to override get_needed_components by specifying the types in the parameter pack */
--- entity_system/world.h
+++ entity_system/world.h
@@ -12,6 +12,13 @@ namespace augmentations {
 	namespace entity_system {
 		class processing_system;
 		class world {
+			
+			template <typename message>
+			class message_queue {
+			public:
+				static std::vector<message> messages;
+			};
+
 			friend class entity;
 			boost::object_pool<entity> entities;
 			std::unordered_map<size_t, boost::pool<>> size_to_container;
@@ -22,14 +29,19 @@ namespace augmentations {
 			boost::pool<>& get_container_for_type(type_hash hash);
 			boost::pool<>& get_container_for_type(const base_type& type);
 			
-			template<typename type>
-			boost::pool<>& get_container_for_type() {
-				return get_container_for_size(sizeof(type));
+			std::vector<processing_system*> systems;
+		public:
+
+			template <typename message>
+			void post_message(const message& message_object) {
+				message_queue<message>::messages.push_back(message_object);
 			}
 
-			std::vector<processing_system*> systems;
+			template <typename message>
+			std::vector<message>& get_message_queue() {
+				return message_queue<message>::messages;
+			}
 
-		public:
 			void add_system(processing_system*); 
 
 			world();
