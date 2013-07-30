0b965b498945b1aa3e5af458a8fe35743ad12254
daedalus <unknownunreleased@gmail.com>
2013-07-30 20:27:59 +0200

    constructor, unit scallin

--- SDK/topdown/components/physics_component.h
+++ SDK/topdown/components/physics_component.h
@@ -2,10 +2,12 @@
 #include <Box2D\Box2D.h>
 
 #include "../../../entity_system/entity_system.h"
+#define PIXELS_TO_METERS 1.0/100
+#define METERS_TO_PIXELS 100.0
 
 namespace components {
 	struct physics : public augmentations::entity_system::component {
 		b2Body* body;
-		physics() : body(nullptr) {}
+		physics(b2Body* body) : body(body) {}
 	};
 }
