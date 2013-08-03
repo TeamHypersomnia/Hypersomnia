f60a1d81f76bd980b49237618e11ff9d68397822
geneotech <unknownunreleased@gmail.com>
2013-08-03 22:00:48 +0200

    introduced simple input contexts

--- SDK/topdown/systems/input_system.cpp
+++ SDK/topdown/systems/input_system.cpp
@@ -1,31 +1,77 @@
+#include "window_framework/window.h"
+#include "entity_system/entity.h"
+
 #include "input_system.h"
-#include "../../../window_framework/window.h"
 
 input_system::input_system(window::glwindow& input_window, bool& quit_flag) : input_window(input_window), quit_flag(quit_flag) {
 
 }
 
+void input_system::post(messages::intent_message incoming_event, world& owner) {
+	auto& m = input_window.events.mouse;
+	incoming_event.mouse_pos = m.pos;
+	incoming_event.mouse_rel = m.rel;
+
+	for (auto it = targets.begin(); it != targets.end(); ++it) {
+		if ((*it)->get<components::input>().intents.find(incoming_event.type)) {
+			incoming_event.subject = *it;
+			owner.post_message(incoming_event);
+		}
+	}
+}
+
+bool input_system::post_intent_from_raw_id(world& owner, const context& active_context, unsigned id, bool state) {
+	auto input_map = active_context.raw_id_to_intent;
+	auto found_intent = input_map.find(id);
+
+	if (found_intent != input_map.end()) {
+		post(messages::intent_message((*found_intent).second, state), owner);
+		return true;
+	}
+	
+	return false;
+}
+
 void input_system::process_entities(world& owner) {
+	using namespace messages;
+	using namespace window::event;
+	using namespace keys;
+
 	window::event::message msg;
 	unsigned incoming_event = 0;
 
-	while (input_window.poll_events(msg)) 
-	{
-		if(msg == window::event::close) {
+	while (input_window.poll_events(msg)) {
+
+		if (msg == window::event::close) {
 			quit_flag = true;
 		}
 
-		if(msg == window::event::key::down || msg == window::event::key::up) {
-			if(input_window.events.key == window::event::keys::ESC)
-				quit_flag = true;
-		}
+		for (auto it = active_contexts.begin(); it != active_contexts.end(); ++it) {
+			bool succesfully_mapped = false;
 
-		for(auto it = targets.begin(); it != targets.end(); ++it) {
-			auto& events = (*it)->get<components::input>();
-			if (events.find(incoming_event)) {
-				owner.post_message(incoming_event);
+			if (msg == key::down) {
+				if (input_window.events.key == ESC)
+					succesfully_mapped = quit_flag = true;
+				else
+					succesfully_mapped = post_intent_from_raw_id(owner, **it, input_window.events.key, true);
 			}
-		}
-	}
 
+			else if (msg == key::up) 
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, input_window.events.key, false);
+
+			else if (msg == mouse::ldown)
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, mouse::ldown, true);
+			else if (msg == mouse::lup)
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, mouse::ldown, false);
+			else if (msg == mouse::rdown)
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, mouse::rdown, true);
+			else if (msg == mouse::rup)
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, mouse::rdown, false);
+			else
+				succesfully_mapped = post_intent_from_raw_id(owner, **it, msg, true);
+
+			if (succesfully_mapped) break;
+		}
+
+	}
 }
--- SDK/topdown/systems/input_system.h
+++ SDK/topdown/systems/input_system.h
@@ -1,6 +1,7 @@
 #pragma once
-#include "../../../entity_system/entity_system.h"
+#include "entity_system/processing_system.h"
 #include "../components/input_component.h"
+#include "../messages/intent_message.h"
 
 using namespace augmentations;
 using namespace entity_system;
@@ -10,12 +11,35 @@ namespace augmentations {
 		class glwindow;
 	}
 }
+/* input to inna domena eventów bo mamy taki sam payload a masa zupelnie co innego znaczacych enumow 
+zwykle message to beda po prostu message_component i sorted vector przyjmowanych enumow
 
+rozwazyc generyczny message component a w bazowym message enum type
+key message z booleanem
+mouse message z vec2 i juz jebac ze sean proponowal w jednym to trzymac
+
+ew jakas hybryda ze obok siebie istnieja message i input componenty
+
+jednak bedzie input component jako normalny component bo wlasnie o to chodzi w componentach ze one serve purpose of grouping
+a tak to bys musial zapierdalac po wszystkich ktore maja message component a to by bylo chujowe
+
+*/
 struct input_system : public processing_system_templated<components::input> {
 	bool& quit_flag;
 	
 	void process_entities(world&) override;
 	
+	struct context {
+		std::unordered_map<unsigned, messages::intent_message::intent> raw_id_to_intent;
+	};
+
+	std::vector<context*> active_contexts;
+
 	window::glwindow& input_window;
 	input_system(window::glwindow&, bool& quit_flag);
+
+	/* returns true if successfully mapped raw input to a high level intent */
+	bool post_intent_from_raw_id(world& owner, const context&, unsigned id, bool state = true);
+
+	void post(messages::intent_message incoming_event, world& owner);
 };
