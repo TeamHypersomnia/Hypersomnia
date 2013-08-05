b1a1e42fc07038ee426c5345221635d905e78463
geneotech <unknownunreleased@gmail.com>
2013-08-05 19:26:57 +0200

    physics system now reacts to moved_message

---
+++ SDK/topdown/messages/moved_messsage.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "message.h"
+
+namespace messages {
+	struct moved_message : public message {
+		/* new transform can be obtained from subject */
+	};
+}
--- SDK/topdown/systems/physics_system.cpp
+++ SDK/topdown/systems/physics_system.cpp
@@ -1,5 +1,6 @@
 #include "physics_system.h"
 #include "entity_system/entity.h"
+#include "../messages/moved_messsage.h"
 
 physics_system::physics_system() : accumulator(30.0, 1), 
 	//world(b2Vec2(0.f, 0.f)) 
@@ -8,8 +9,25 @@ physics_system::physics_system() : accumulator(30.0, 1),
 	b2world.SetAutoClearForces(false);
 }
 
-void physics_system::process_entities(world&) {
+void physics_system::process_entities(world& owner) {
 	const unsigned steps = accumulator.update_and_extract_steps();
+
+	/* we will update body's transforms HERE according to the ENTITY_MOVED message
+	bitsquid's events are particularly well when message can be processed "within the same domain"
+	whereas when it comes to registrations to entity-specific notations, there is probably some overhead imposed with searching the matching subscriber
+	*/
+
+	auto events = owner.get_message_queue<messages::moved_message>();
+
+	for (auto it = events.begin(); it != events.end(); ++it) {
+		auto physics = (*it).subject->find<components::physics>();
+		auto transform = (*it).subject->find<components::transform>();
+
+		if (!physics || !transform) continue;
+		
+		physics->body->SetTransform(transform->current.pos, transform->current.rotation);
+	}
+
 
 	for (unsigned i = 0; i < steps; ++i) {
 		int32 velocityIterations = 8;
@@ -18,6 +36,9 @@ void physics_system::process_entities(world&) {
 		reset_states();
 		b2world.Step(accumulator.per_second(), velocityIterations, positionIterations);
 	}
+
+	/* note we DON'T send moved_message for every processed physics entity, because if something wants to keep track of an entity's position it can
+	hold its reference and poll the position every frame */
 
 	b2world.ClearForces();
 	smooth_states();
