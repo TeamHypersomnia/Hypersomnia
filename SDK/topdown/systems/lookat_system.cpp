159d9e13a4b99058e88cc206d6dc9affb7f7086c
geneotech <unknownunreleased@gmail.com>
2013-08-10 15:40:31 +0200

    introduced lookat system

---
+++ SDK/topdown/systems/lookat_system.cpp
@@ -0,0 +1,17 @@
+#include "lookat_system.h"
+#include "../messages/moved_message.h"
+
+void lookat_system::process_entities(world& owner) {
+	for (auto it = targets.begin(); it != targets.end(); ++it) {
+		auto& lookat = (*it)->get<components::lookat>();
+		
+		auto target_transform = lookat.target->find<components::transform>();
+		
+		if (target_transform) {
+			auto& transform = (*it)->get<components::transform>();
+
+			transform.current.rotation = (target_transform->current.pos - transform.current.pos).get_radians();
+			owner.post_message(messages::moved_message(*it));
+		}
+	}
+}
