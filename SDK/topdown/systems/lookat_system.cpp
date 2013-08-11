ede16da5ed44ef4cf368415aa7005bfe15be6a96
geneotech <unknownunreleased@gmail.com>
2013-08-11 03:04:10 +0200

    range based lookat system

--- SDK/topdown/systems/lookat_system.cpp
+++ SDK/topdown/systems/lookat_system.cpp
@@ -2,16 +2,16 @@
 #include "../messages/moved_message.h"
 
 void lookat_system::process_entities(world& owner) {
-	for (auto it = targets.begin(); it != targets.end(); ++it) {
-		auto& lookat = (*it)->get<components::lookat>();
+	for (auto it : targets) {
+		auto& lookat = it->get<components::lookat>();
 		
 		auto target_transform = lookat.target->find<components::transform>();
 		
 		if (target_transform) {
-			auto& transform = (*it)->get<components::transform>();
+			auto& transform = it->get<components::transform>();
 
 			transform.current.rotation = (target_transform->current.pos - transform.current.pos).get_radians();
-			owner.post_message(messages::moved_message(*it));
+			owner.post_message(messages::moved_message(it));
 		}
 	}
 }
