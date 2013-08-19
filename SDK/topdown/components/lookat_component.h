bac20db4776ed4b5280bf12c1253a0ba11fdde81
geneotech <unknownunreleased@gmail.com>
2013-08-19 15:48:32 +0200

    lookat now is able to face velocity and not only position

--- SDK/topdown/components/lookat_component.h
+++ SDK/topdown/components/lookat_component.h
@@ -3,8 +3,13 @@
 
 namespace components {
 	struct lookat : public augmentations::entity_system::component {
+		enum class chase_type {
+			POSITION,
+			VELOCITY
+		} type;
+
 		augmentations::entity_system::entity* target;
 
-		lookat(augmentations::entity_system::entity* target) : target(target) {}
+		lookat(augmentations::entity_system::entity* target, chase_type type = chase_type::POSITION) : target(target), type(type) {}
 	};
 }
--- SDK/topdown/systems/lookat_system.cpp
+++ SDK/topdown/systems/lookat_system.cpp
@@ -1,17 +1,17 @@
 #include "lookat_system.h"
 #include "../messages/moved_message.h"
+#include "../components/physics_component.h"
 
 void lookat_system::process_entities(world& owner) {
 	for (auto it : targets) {
 		auto& lookat = it->get<components::lookat>();
-		
-		auto target_transform = lookat.target->find<components::transform>();
-		
-		if (target_transform) {
-			auto& transform = it->get<components::transform>();
+		auto& transform = it->get<components::transform>();
 
-			transform.current.rotation = (target_transform->current.pos - transform.current.pos).get_radians();
-			//owner.post_message(messages::moved_message(it));
+		if (lookat.type == components::lookat::chase_type::POSITION) {
+			transform.current.rotation = (lookat.target->get<components::transform>().current.pos - transform.current.pos).get_radians();
+		}
+		else if (lookat.type == components::lookat::chase_type::VELOCITY) {
+			transform.current.rotation = vec2<>(lookat.target->get<components::physics>().body->GetLinearVelocity()).get_radians();
 		}
 	}
 }
