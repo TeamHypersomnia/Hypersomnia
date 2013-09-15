bcc38cd993a848f6c39abbab8c87b159ce5c96c6
geneotech <unknownunreleased@gmail.com>
2013-09-15 23:40:28 +0200

    moved particle emission class

--- SDK/topdown/components/particle_emitter_component.h
+++ SDK/topdown/components/particle_emitter_component.h
@@ -1,18 +1,75 @@
 #pragma once
-#include <unordered_map>
+#include "utility/map_wrapper.h"
 #include "utility/timer.h"
 #include "entity_system/entity_ptr.h"
 
 #include "../messages/particle_burst_message.h"
-#include "particle_group_component.h"
 #include "chase_component.h"
+#include "../renderable.h"
 
 namespace components {
 	struct particle_emitter : public augmentations::entity_system::component {
-		typedef std::unordered_map<messages::particle_burst_message::burst_type, std::vector<particle_group::emission>> subscribtion;
-		subscribtion* available_emissions;
+		struct particle {
+			augmentations::vec2<> pos, vel, acc;
+			sprite face;
+			float rotation;
+			float rotation_speed;
+			float linear_damping;
+			float angular_damping;
+			float lifetime_ms, max_lifetime_ms;
+			bool should_disappear;
+			particle() : face(nullptr), lifetime_ms(0.f), should_disappear(true), rotation(0.f), rotation_speed(0.f) {}
+		};
 
-		particle_emitter(subscribtion* available_emissions) 
-			: available_emissions(available_emissions) {}
+		struct emission {
+			enum type {
+				BURST,
+				STREAM
+			} type;
+
+			float spread_radians;
+
+			std::pair<float, float>
+				velocity,
+				angular_velocity,
+				particles_per_sec,
+				stream_duration_ms,
+				particle_lifetime_ms,
+				size_multiplier,
+				acceleration;
+
+			std::pair<unsigned, unsigned>
+				particles_per_burst;
+
+			float initial_rotation_variation;
+			bool randomize_acceleration;
+
+			augmentations::vec2<> offset;
+			float angular_offset;
+
+			std::vector<particle> particle_templates;
+			unsigned particle_group_layer;
+
+			void add_particle_template(const particle& p) {
+				particle_templates.push_back(p);
+			}
+
+			emission() : acceleration(std::make_pair(0.f, 0.f)), randomize_acceleration(false) {}
+		};
+
+		typedef std::vector<emission> particle_effect;
+
+		static void add(particle_effect* self, emission object) {
+			self->push_back(object);
+		}
+
+		typedef augmentations::util::map_wrapper<
+			messages::particle_burst_message::burst_type, particle_effect
+		> subscribtion;
+
+		subscribtion* available_particle_effects;
+
+		particle_emitter(subscribtion* available_particle_effects = nullptr)
+			: available_particle_effects(available_particle_effects) {}
 	};
 }
--- SDK/topdown/components/particle_group_component.h
+++ SDK/topdown/components/particle_group_component.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <vector>
-#include "entity_system/component.h"
 #include "math/vec2d.h"
-#include "../renderable.h"
+#include "particle_emitter_component.h"
 
 class physics_system;
 class render_system;
@@ -12,56 +11,9 @@ class particle_emitter_system;
 namespace components {
 	/* hack - the only component that has logic */
 	struct particle_group : public augmentations::entity_system::component, public renderable {
-		struct particle {
-			augmentations::vec2<> pos, vel, acc;
-			sprite face;
-			float rotation;
-			float rotation_speed;
-			float linear_damping;
-			float angular_damping;
-			float lifetime_ms, max_lifetime_ms;
-			bool should_disappear;
-			particle() : face(nullptr), lifetime_ms(0.f), should_disappear(true), rotation(0.f), rotation_speed(0.f) {}
-		};
-
-		struct emission {
-			enum class type {
-				BURST,
-				STREAM
-			} type;
-
-			float spread_radians;
-			float velocity_min;
-			float velocity_max;
-			float angular_velocity_min;
-			float angular_velocity_max;
-			float particles_per_sec_min;
-			float particles_per_sec_max;
-			float stream_duration_ms_min;
-			float stream_duration_ms_max;
-			float particle_lifetime_ms_min;
-			float particle_lifetime_ms_max;
-			float size_multiplier_min;
-			float size_multiplier_max;
-			float initial_rotation_variation;
-			unsigned particles_per_burst_min;
-			unsigned particles_per_burst_max;
-
-			bool randomize_acceleration;
-			float acc_min;
-			float acc_max;
-
-			augmentations::vec2<> offset;
-			float angular_offset;
-
-			std::vector<particle_group::particle> particle_templates;
-			unsigned particle_group_layer;
-
-			emission() : acc_min(0.f), acc_max(0.f), randomize_acceleration(false) {}
-		};
 		physics_system* optional_physics;
 
-		std::vector<particle> particles;
+		std::vector<particle_emitter::particle> particles;
 
 		particle_group() 
 			: optional_physics(nullptr), destroy_when_empty(true), stream_info(nullptr), stream_lifetime_ms(0.f), stream_particles_to_spawn(0.f) {}
@@ -82,6 +34,6 @@ namespace components {
 		float stream_max_lifetime_ms;
 		float stream_particles_to_spawn;
 
-		emission* stream_info;
+		particle_emitter::emission* stream_info;
 	};
 }
