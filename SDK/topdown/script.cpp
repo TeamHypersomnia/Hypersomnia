03f9153385f39873515bad2c6de04e0779ea0b46
geneotech <unknownunreleased@gmail.com>
2013-09-15 23:42:12 +0200

    introduced script reloader

--- SDK/topdown/script.cpp
+++ SDK/topdown/script.cpp
@@ -5,7 +5,7 @@ extern "C" {
 #include <lua\lauxlib.h>
 }
 
-script::script() : needs_reloading(false), is_associated_string_filename(false) {
+script::script() : needs_reloading(false), is_associated_string_filename(false), reload_scene_when_modified(true) {
 
 }
 
@@ -15,7 +15,45 @@ void script::associate_string(const std::string& str) {
 	needs_reloading = true;
 }
 
-void script::associate_filename(const std::string& str) {
+script::reloader script::script_reloader;
+
+std::vector<script*> script::reloader::get_modified_script_files() {
+	std::vector<script*> output;
+
+	if (WaitForSingleObjectEx(changes.GetWaitHandle(), 0, false) == WAIT_OBJECT_0) {
+		if (changes.CheckOverflow()) {
+			throw std::exception("file change command buffer overflow");
+		}
+		else {
+			DWORD dwAction;
+			CStringW wstrFilename;
+			
+			while (changes.Pop(dwAction, wstrFilename)) {
+				if (dwAction == FILE_ACTION_MODIFIED) {
+					auto it = filename_to_script.find(std::wstring(wstrFilename));
+
+					if(it != filename_to_script.end()) 
+						output.push_back((*it).second);
+				}
+			}
+		}
+	}
+
+	return output;
+}
+
+void script::reloader::add_directory(std::wstring& wdir, bool subtree) {
+	//std::wstring wdir(directory.begin(), directory.end());
+	changes.AddDirectory(wdir.c_str(), subtree, FILE_NOTIFY_CHANGE_LAST_WRITE);
+}
+
+void script::associate_filename(const std::string& str, bool register_for_file_reloading) {
+	if (is_associated_string_filename) 
+		script_reloader.filename_to_script.erase(std::wstring(associated_string.begin(), associated_string.end()));
+
+	if (register_for_file_reloading) 
+		script_reloader.filename_to_script[std::wstring(str.begin(), str.end())] = this;
+
 	associated_string = str;
 	is_associated_string_filename = true;
 	needs_reloading = true;
@@ -57,8 +95,12 @@ const char* lua_reader(lua_State *L, void *data, size_t *sz) {
 		else return nullptr;
 }
 
+void script::set_out_of_date() {
+	needs_reloading = true;
+}
+
 std::string script::compile(lua_State* lua_state) {
-	if (needs_reloading || is_associated_string_filename) {
+	if (needs_reloading) {
 		int result = LUA_OK;
 
 		if (is_associated_string_filename)
@@ -79,6 +121,9 @@ std::string script::compile(lua_State* lua_state) {
 }
 
 std::string script::call(lua_State* lua_state) {
+	auto compilation_error = compile(lua_state);
+	if (!compilation_error.empty()) return compilation_error;
+
 	auto info = std::make_pair(true, &bytecode);
 	
 	if (lua_load(lua_state, lua_reader, &info, "scriptname", "b") != LUA_OK)
--- SDK/topdown/script.h
+++ SDK/topdown/script.h
@@ -1,6 +1,9 @@
 #pragma once
+#define UNICODE
 #include <string> 
 #include <vector>
+#include <unordered_map>
+#include "readdir/ReadDirectoryChanges.h"
 
 struct lua_State;
 class script {
@@ -11,14 +14,29 @@ class script {
 
 	std::vector<char> bytecode;
 public:
+	class reloader {
+		CReadDirectoryChanges changes;
+	public:
+		std::unordered_map<std::wstring, script*> filename_to_script;
+		std::vector<script*> get_modified_script_files();
+		void add_directory(std::wstring& directory, bool subtree);
+	};
+
+	static reloader script_reloader;
+
 	script();
 
 	void associate_string(const std::string&);
-	void associate_filename(const std::string&);
+	void associate_filename(const std::string&, 
+		bool register_for_file_reloading = true);
 	
 	const std::string& get_associated_string() const;
 	bool is_filename() const;
 	const std::vector<char>& get_bytecode() const;
+
+	bool reload_scene_when_modified;
+
+	void set_out_of_date();
 
 	/* refreshes bytecode, 
 	does not cause recompilation if associated string was not modified
