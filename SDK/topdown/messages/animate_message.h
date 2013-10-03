574736d0dba0152585d9ec6b4f1799bbdf501808
geneotech <unknownunreleased@gmail.com>
2013-10-03 07:02:03 +0200

    direct set_animation option

--- SDK/topdown/messages/animate_message.h
+++ SDK/topdown/messages/animate_message.h
@@ -2,6 +2,10 @@
 #include "message.h"
 #include "utility/sorted_vector.h"
 
+namespace resources {
+	struct animation;
+}
+
 namespace messages {
 	struct animate_message : public message {
 		enum animation {
@@ -22,9 +26,12 @@ namespace messages {
 		float speed_factor;
 		int animation_priority;
 
-		animate_message() : change_speed(false), speed_factor(1.f), change_animation(false), preserve_state_if_animation_changes(false), animation_priority(0) {}
+		/* if nullptr, will take animation_type as key for animation_subscribtion map in animate_component */
+		resources::animation* set_animation;
+
+		animate_message() : set_animation(nullptr), change_speed(false), speed_factor(1.f), change_animation(false), preserve_state_if_animation_changes(false), animation_priority(0) {}
 		animate_message(animation animation_type, type message_type, bool override_speed, float speed) 
-			: animation_type(animation_type), message_type(message_type), change_speed(change_speed), speed_factor(speed_factor), animation_priority(0), preserve_state_if_animation_changes(false)
+			: set_animation(nullptr), animation_type(animation_type), message_type(message_type), change_speed(change_speed), speed_factor(speed_factor), animation_priority(0), preserve_state_if_animation_changes(false)
 		{}
 	};
 }
