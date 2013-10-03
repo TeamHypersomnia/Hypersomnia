14f6a08812adbc28b705fcd01500a2e78a02ad08
geneotech <unknownunreleased@gmail.com>
2013-10-03 07:03:53 +0200

    drawing policy changed

--- SDK/topdown/components/particle_group_component.h
+++ SDK/topdown/components/particle_group_component.h
@@ -3,16 +3,22 @@
 #include "math/vec2d.h"
 #include "particle_emitter_component.h"
 
+#include "../resources/render_info.h"
+
 class physics_system;
 class particle_group_system;
 class particle_emitter_system;
 
+namespace resources {
+	struct emission;
+}
+
 namespace components {
 	/* hack - the only component that has logic */
-	struct particle_group : public augmentations::entity_system::component {
+	struct particle_group : public augmentations::entity_system::component, public resources::renderable {
 		physics_system* optional_physics;
 
-		std::vector<particle_emitter::particle> particles;
+		std::vector<resources::particle> particles;
 
 		particle_group() 
 			: optional_physics(nullptr), destroy_when_empty(true), stream_info(nullptr), stream_lifetime_ms(0.f), stream_particles_to_spawn(0.f) {}
@@ -21,6 +27,10 @@ namespace components {
 		friend class particle_group_system;
 		friend class particle_emitter_system;
 
+		virtual void draw(resources::buffer&, const components::transform&, vec2<> camera_pos) override;
+		virtual bool is_visible(rects::xywh visibility_aabb, const components::transform&) override;
+		virtual b2Body* create_body(entity_system::entity& subject, b2World& b2world, b2BodyType type) override;
+
 		/* only used by subject stream to indicate that it will no longer emit particles */
 		bool destroy_when_empty;
 
@@ -28,6 +38,6 @@ namespace components {
 		float stream_max_lifetime_ms;
 		float stream_particles_to_spawn;
 
-		particle_emitter::emission* stream_info;
+		resources::emission* stream_info;
 	};
 }
