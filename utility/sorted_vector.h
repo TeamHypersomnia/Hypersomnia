b1d68309e2111e414f4e6521ffa0d0f37dab4961
daedalus <unknownunreleased@gmail.com>
2013-10-14 15:13:08 +0200

    corrected sorted_vector api for entity needs

--- utility/sorted_vector.h
+++ utility/sorted_vector.h
@@ -4,19 +4,23 @@
 namespace augmentations {
 	namespace util {
 		template<typename value>
-		class sorted_vector {
+		struct sorted_vector {
 			std::vector<value> raw;
-		public:
+			
 			void add(const value& val) {
 				raw.push_back(val);
 				std::sort(raw.begin(), raw.end());
 			}
 
-			void remove(const value& val) {
+			bool remove(const value& val) {
 				auto it = std::lower_bound(raw.begin(), raw.end(), val);
 
-				if (it != raw.end() && *it == val)
+				if (it != raw.end() && *it == val) {
 					raw.erase(it);
+					return true;
+				}
+
+				return false;
 			}
 
 			bool find(const value& key) const {
@@ -25,13 +29,41 @@ namespace augmentations {
 
 			value* get(const value& key) {
 				auto it = std::lower_bound(raw.begin(), raw.end(), key);
-				if (it == raw.end() || !(*it < key))
-					return &*it;
-				return nullptr;
+				if (it == raw.end() || *it != key)
+					return nullptr;
+				return &*it;
+			}
+		};
+
+		template<typename key_type, typename value_type>
+		struct sorted_vector_map {
+			std::vector<std::pair<key_type, value_type>> raw;
+
+			void add(const key_type& key, const value_type& val) {
+				raw.push_back(std::make_pair(key, val));
+				std::sort(raw.begin(), raw.end());
 			}
 
-			const std::vector<value>& get_vector() const {
-				return raw;
+			bool remove(const key_type& key) {
+				auto it = std::lower_bound(raw.begin(), raw.end(), std::make_pair(key, value_type()));
+
+				if (it != raw.end() && *it == val) {
+					raw.erase(it);
+					return true;
+				}
+
+				return false;
+			}
+
+			bool find(const key_type& key) const {
+				return std::binary_search(raw.begin(), raw.end(), std::make_pair(key, value_type()));
+			}
+
+			value_type* get(const key_type& key) {
+				auto it = std::lower_bound(raw.begin(), raw.end(), std::make_pair(key, value_type()));
+				if (it == raw.end() || (*it).first != key)
+					return nullptr;
+				return &(*it).second;
 			}
 		};
 	}
