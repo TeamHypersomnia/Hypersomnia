17d47289b6e908c57f8cc85dd37f11cf8b162897
geneotech <unknownunreleased@gmail.com>
2013-10-31 20:37:29 +0100

    fbo draft

---
+++ SDK/topdown/resources/fbo.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "fbo.h"
+
+GLuint fbo::currently_bound_fbo = 0u;
+
+fbo::fbo() : created(false), fboId(0u), textureId(0u) {
+}
+
+fbo::fbo(int width, int height) : created(false) {
+	create(width, height);
+}
+
+void fbo::create(int w, int h) {
+	if (created) return;
+
+	created = true;
+	width = w;
+	height = h;
+
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE); // automatic mipmap
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
+		GL_RGBA, GL_UNSIGNED_BYTE, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glGenFramebuffers(1, &fboId);
+	use();
+
+	// attach the texture to FBO color attachment point
+	glFramebufferTexture2D(GL_FRAMEBUFFER,        // 1. fbo target: GL_FRAMEBUFFER 
+		GL_COLOR_ATTACHMENT0,  // 2. attachment point
+		GL_TEXTURE_2D,         // 3. tex target: GL_TEXTURE_2D
+		textureId,             // 4. tex ID
+		0);                    // 5. mipmap level: 0(base)
+
+	// check FBO status
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+		std::cout << "an error occured during FBO creation" << std::endl;
+}
+
+void fbo::use() {
+	glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+	currently_bound_fbo = fboId;
+}
+
+void fbo::guarded_use() {
+	if (currently_bound_fbo != fboId)
+		use();
+}
+
+void fbo::use_default() {
+	if (currently_bound_fbo != 0) {
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		currently_bound_fbo = 0;
+	}
+}
+
+int fbo::get_width() const {
+	return width;
+}
+
+int fbo::get_height() const {
+	return height;
+}
+
+void fbo::destroy() {
+	created = false;
+	width = 0;
+	height = 0;
+	fboId = 0;
+	textureId = 0;
+	glDeleteFramebuffers(1, &fboId);
+	glDeleteTextures(1, &textureId);
+}
+
+fbo::~fbo() {
+	destroy();
+}
+
+GLuint fbo::getTextureId() const {
+	return textureId;
+}
---
+++ SDK/topdown/resources/fbo.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class fbo {
+	GLuint fboId, textureId, width, height;
+	bool created;
+	fbo& operator=(const fbo&) {}
+	fbo(const fbo&) {}
+
+	static GLuint currently_bound_fbo;
+public:
+	fbo(); ~fbo();
+	fbo(int width, int height);
+	void create(int width, int height);
+	void destroy();
+
+	void use();
+	void guarded_use();
+
+	int get_width() const, get_height() const;
+	GLuint getTextureId() const;
+
+	static void use_default();
+};
