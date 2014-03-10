921f77cc501287bb77c972530965d6db773635e0
geneotech <unknownunreleased@gmail.com>
2014-03-11 00:08:52 +0100

    bound int rect ltrb

--- game_framework/bindings/rect_ltrb_binding.cpp
+++ game_framework/bindings/rect_ltrb_binding.cpp
@@ -15,6 +15,20 @@ namespace bindings {
 			.def_readwrite("r", &rects::ltrb<float>::r)
 			.def_readwrite("b", &rects::ltrb<float>::b)
 			.property("w", (float (rects::ltrb<float>::*)() const)&rects::ltrb<float>::w, (void (rects::ltrb<float>::*)(float)) &rects::ltrb<float>::w)
-			.property("h", (float (rects::ltrb<float>::*)() const)&rects::ltrb<float>::h, (void (rects::ltrb<float>::*)(float)) &rects::ltrb<float>::h);
+			.property("h", (float (rects::ltrb<float>::*)() const)&rects::ltrb<float>::h, (void (rects::ltrb<float>::*)(float)) &rects::ltrb<float>::h),
+
+
+			luabind::class_<rects::ltrb<int>>("rect_ltrb_i")
+			.def(luabind::constructor<const rects::xywh<int>&>())
+			.def(luabind::constructor<int, int, int, int>())
+			.def_readwrite("l", &rects::ltrb<int>::l)
+			.def_readwrite("t", &rects::ltrb<int>::t)
+			.def_readwrite("r", &rects::ltrb<int>::r)
+			.def_readwrite("b", &rects::ltrb<int>::b)
+			.property("w", (int (rects::ltrb<int>::*)() const)&rects::ltrb<int>::w, (void (rects::ltrb<int>::*)(int)) &rects::ltrb<int>::w)
+			.property("h", (int (rects::ltrb<int>::*)() const)&rects::ltrb<int>::h, (void (rects::ltrb<int>::*)(int)) &rects::ltrb<int>::h)
+
+
+			;
 	}
 }
