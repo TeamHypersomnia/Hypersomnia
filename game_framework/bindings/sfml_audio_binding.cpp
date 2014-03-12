c664b4aafb3d120264a22108035416e96037f731
geneotech <unknownunreleased@gmail.com>
2014-03-12 14:06:59 +0100

    thorough bindings for sfml audio

--- game_framework/bindings/sfml_audio_binding.cpp
+++ game_framework/bindings/sfml_audio_binding.cpp
@@ -4,17 +4,28 @@
 
 #include <SFML/Audio.hpp>
 
+bool has_sound_stopped_playing(sf::Sound* s) {
+	return s->getStatus() == sf::Sound::Status::Stopped;
+}
+
 namespace bindings {
 	luabind::scope _sfml_audio() {
 		return
 			(
+			luabind::def("has_sound_stopped_playing", has_sound_stopped_playing),
+
 			luabind::class_<sf::Music>("sfMusic")
 			.def(luabind::constructor<>())
 			.def("openFromFile", &sf::Music::openFromFile)
 			.def("play", &sf::Music::play)
 			.def("pause", &sf::Music::pause)
+			.def("stop", &sf::Music::stop)
 			.def("setVolume", &sf::Music::setVolume)
 			.def("setPitch", &sf::Music::setPitch)
+			.def("setPlayingOffset", &sf::Music::setPlayingOffset)
+			.def("setLoop", &sf::Music::setLoop)
+			.def("setRelativeToListener", &sf::Music::setRelativeToListener)
+			.def("setPlayingOffset", &sf::Music::setPlayingOffset)
 			,
 
 			luabind::class_<sf::SoundBuffer>("sfSoundBuffer")
@@ -24,11 +35,16 @@ namespace bindings {
 
 			luabind::class_<sf::Sound>("sfSound")
 			.def(luabind::constructor<>())
-			.def("play", &sf::Sound::play)
+			.def("setAttenuation", &sf::Sound::setAttenuation)
+			.def("setBuffer", &sf::Sound::setBuffer)
+			.def("setMinDistance", &sf::Sound::setMinDistance)
+			.def("setRelativeToListener", &sf::Sound::setRelativeToListener)
+			.def("setPlayingOffset", &sf::Sound::setPlayingOffset)
 			.def("pause", &sf::Sound::pause)
 			.def("setLoop", &sf::Sound::setLoop)
 			.def("setVolume", &sf::Sound::setVolume)
 			.def("setPitch", &sf::Sound::setPitch)
+			.def("play", &sf::Sound::play)
 			.def("setPosition", (void (sf::Sound::*)(float, float, float))(&sf::Sound::setPosition))
 			);
 	}
