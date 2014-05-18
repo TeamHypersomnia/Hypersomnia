1d2981cbd8bc1981036665ea289970ab28828426
geneotech <unknownunreleased@gmail.com>
2014-05-18 22:22:13 +0200

    removed legacy code from repo #2

--- hp/scripts/hp_init.lua
+++
@@ -1,8 +0,0 @@
-
--- only initialize resources
-
-dofile "hp\\scripts\\resources\\layers.lua"
-dofile "hp\\scripts\\resources\\textures.lua"
-dofile "hp\\scripts\\resources\\animations.lua"
-
-dofile "hp\\scripts\\sample_scenes\\soldier_ai.lua"
