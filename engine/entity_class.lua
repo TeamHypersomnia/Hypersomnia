6f2006041c550fab3f02aa26ce79e635fe062630
geneotech <unknownunreleased@gmail.com>
2014-05-18 22:24:12 +0200

    changed responsibility of entity creation

--- engine/entity_class.lua
+++ engine/entity_class.lua
@@ -1,9 +1,7 @@
 entity_class = inherits_from {}
 
-function entity_class:constructor(parent_entity)
-	self.parent_entity = parent_entity
-	self.parent_group = get_group_by_entity(parent_entity:get())
-end
+-- to be called exclusively by entity_system:create_entity_table
+function entity_class:constructor() end
 
 function get_self(entity)
 	return entity.scriptable.script_data
@@ -50,50 +48,4 @@ function entity_class:try_module_method(module_name, method_name, ...)
 	-- call method
 		m[method_name](m, ...)
 	end
-end
-
-entity_basic_scriptable_info = create_scriptable_info {
-	scripted_events = {
-		[scriptable_component.LOOP] = function (subject, is_substepping)
-			local my_self = get_self(subject)
-			
-			if is_substepping then
-				my_self:substep()
-			else
-				my_self:loop()
-			end
-		end,
-		
-		[scriptable_component.DAMAGE_MESSAGE] = function (message)
-			table.insert(global_message_table[scriptable_component.DAMAGE_MESSAGE], message)
-		end,
-		
-		[scriptable_component.INTENT_MESSAGE] = function (message)
-			table.insert(global_message_table[scriptable_component.INTENT_MESSAGE], message)
-		end
-	}
-}
-
--- merely generates an entity object inside scriptable
-function generate_entity_object(what_entity, what_class, ...)
-	if what_class == nil then what_class = entity_class end
-	
-	-- if scriptable component does not exist, add a new one
-	if what_entity:get().scriptable == nil then what_entity:get():add(scriptable_component()) end
-	--group_table = archetyped( { body = { scriptable = { } } }, group_table )
-	
-	--print (table.inspect(group_table))
-	--local my_new_entity_group = ptr_create_entity_group (group_table)
-	local new_entity_script_data = what_class:create(what_entity, ...)
-	-- there is no need to override the basic scriptable info as it provides entities with all needed functionality
-	-- and in fact should not be modified
-	--local new_scriptable_info = create_scriptable_info (scriptable_table)
-	
-	
-	what_entity:get().scriptable.script_data = new_entity_script_data
-	what_entity:get().scriptable.available_scripts = entity_basic_scriptable_info
-	
-	table.insert(global_entity_table, new_entity_script_data)
-
-	return new_entity_script_data
 end
--- engine/entity_system.lua
+++ engine/entity_system.lua
@@ -1,14 +1,34 @@
 entity_system = inherits_from {}
 
-function entity_system:constructor()
+function entity_system:constructor(owner_world)
 	self.entity_table = {}
-	self.scriptables_table = {}
 	self.message_table = {}
 	
 	self.message_table[scriptable_component.DAMAGE_MESSAGE] = {}
 	self.message_table[scriptable_component.INTENT_MESSAGE] = {}
-	
-	
+	 
+	self.entity_basic_scriptable_info = create_scriptable_info {
+		scripted_events = {
+			[scriptable_component.LOOP] = function (subject, is_substepping)
+				local my_self = get_self(subject)
+				
+				if is_substepping then
+					my_self:substep()
+				else
+					my_self:loop()
+				end
+			end,
+			
+			[scriptable_component.DAMAGE_MESSAGE] = function (message)
+				table.insert(self.message_table[scriptable_component.DAMAGE_MESSAGE], message)
+			end,
+			
+			[scriptable_component.INTENT_MESSAGE] = function (message)
+				table.insert(self.message_table[scriptable_component.INTENT_MESSAGE], message)
+			end
+		}
+	}
+	 
 	-- user variables
 	self.enable_loop = true
 	self.registered_modules = {}
@@ -25,12 +45,41 @@ function entity_system:constructor()
 		}
 	}
 	
-	self.loop_script_entity = create_entity {
+	self.loop_script_entity = owner_world:create_entity {
 		scriptable = {
 			available_scripts = self.loop_script_info,
 			script_data = self
 		}
 	}
+	
+	self.owner_world = owner_world
+end
+
+-- merely generates an entity object inside scriptable
+function entity_system:create_entity_table(what_entity, what_class, ...)
+	if what_class == nil then what_class = entity_class end
+	
+	-- if scriptable component does not exist, add a new one
+	if what_entity:get().scriptable == nil then what_entity:get():add(scriptable_component()) end
+	--group_table = archetyped( { body = { scriptable = { } } }, group_table )
+	
+	--print (table.inspect(group_table))
+	--local my_new_entity_group = ptr_create_entity_group (group_table)
+	local new_entity_script_data = what_class:create(...)
+	
+	new_entity_script_data.parent_entity = what_entity
+	new_entity_script_data.parent_group = self.owner_world:get_group_by_entity(what_entity:get())
+	
+	-- there is no need to override the basic scriptable info as it provides entities with all needed functionality
+	-- and in fact should not be modified
+	--local new_scriptable_info = create_scriptable_info (scriptable_table)
+	
+	what_entity:get().scriptable.script_data = new_entity_script_data
+	what_entity:get().scriptable.available_scripts = self.entity_basic_scriptable_info
+	
+	table.insert(self.entity_table, new_entity_script_data)
+
+	return new_entity_script_data
 end
 
 function entity_system:process_all_entities(callback)
