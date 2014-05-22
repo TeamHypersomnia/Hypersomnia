2f3bdff6391198fd2a2081f38b736205bfa5bbd8
geneotech <unknownunreleased@gmail.com>
2014-05-22 20:41:10 +0200

    recreated old loop behaviour

--- hypersomnia/scripts/start.lua
+++ hypersomnia/scripts/start.lua
@@ -13,3 +13,20 @@ dofile "hypersomnia\\scripts\\resources\\animations.lua"
 sample_scene = scene_class:create()
 sample_scene:load_map("hypersomnia\\data\\maps\\sample_map", "hypersomnia\\scripts\\loaders\\basic_map_loader")
 sample_scene:set_current()
+
+
+print "Initialization is OK."
+-- main loop
+
+while true do
+	if augmentations_main_loop_callback ~= nil then
+		local result = augmentations_main_loop_callback()
+		if result ~= nil and result ~= 0 then break end
+	end
+	
+	if call_once_after_loop ~= nil then
+		call_once_after_loop()
+		call_once_after_loop = nil
+	end
+end
+
