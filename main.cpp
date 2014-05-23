7f6d6128dfe8c20c7e82f20f5ccf47738b497a87
geneotech <unknownunreleased@gmail.com>
2014-05-23 02:14:29 +0200

    uploaded working C++ project and reorganization

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "stdafx.h"
+#include "game_framework/resources/lua_state_wrapper.h"
+#include "game_framework/game_framework.h"
+
+int main() {
+	framework::init();
+
+	resources::lua_state_wrapper lua_state;
+	lua_state.bind_whole_engine();
+
+	lua_state.dofile("init.lua"); 
+
+	framework::deinit();
+	return 0;
+}  
