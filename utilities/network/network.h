d20216521d858676ded2fa0b6e595b0932f00574
geneotech <unknownunreleased@gmail.com>
2014-05-31 23:17:50 +0200

    packet helper class

--- utilities/network/network.h
+++ utilities/network/network.h
@@ -5,6 +5,47 @@ namespace augs  {
 	namespace network {
 		bool init(), deinit();
 
+		enum io_result {
+			SUCCESSFUL,
+			FAILED,
+			INCOMPLETE
+		};
+
+		struct packet {
+			std::vector<char> data;
+			
+			template <typename T>
+			T read() {
+				if (data.size() < sizeof(T)) 
+					throw std::runtime_error("attempting to read more bytes than buffer has!");
+
+				size_t old_size = data.size();
+				
+				T return_value;
+				memcpy(&return_value, data.data() + old_size - sizeof(T), sizeof(T));
+				
+				data.resize(old_size - sizeof(T));
+
+				return return_value;
+			}
+
+			template <typename T>
+			void write(const T& value) {
+				size_t old_size = data.size();
+
+				data.resize(old_size + sizeof(value));
+				memcpy(data.data() + old_size, &value, sizeof(value));
+			}
+
+			template <>
+			void write<std::string>(const std::string& value) {
+				size_t old_size = data.size();
+
+				data.resize(old_size + value.length());
+				memcpy(data.data() + old_size, value.c_str(), value.length());
+			}
+		};
+
 		struct ip {
 			struct sockaddr_in addr;
 			static int size;
