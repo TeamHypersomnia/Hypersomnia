4a1c82d7282ebc13e4b7e4d16fcfc0cd301e6503
geneotech <unknownunreleased@gmail.com>
2014-06-10 18:58:06 +0200

    minimal connection demo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #pragma once
-#include "stdafx.h"
+#include "RakPeerInterface.h"
+#include "MessageIdentifiers.h"
+#include "BitStream.h"
+#include "RakNetTypes.h"
+
 #include "game_framework/resources/lua_state_wrapper.h"
 #include "game_framework/game_framework.h"
 
-#include "network/udp.h"
+
+enum GameMessages
+{
+	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
+};
+
 
 int main() {
 	framework::init();
@@ -11,26 +20,61 @@ int main() {
 	resources::lua_state_wrapper lua_state;
 	lua_state.bind_whole_engine();
 
-	using namespace augs::network;
-	udp udp_socket;
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
 
-	using namespace augs;
-	if (network::init()) {
-		if (udp_socket.open()) {
-			udp_socket.set_blocking(true);
+	peer->Startup(1, &RakNet::SocketDescriptor(), 1);
+	auto res = peer->Connect("127.0.0.1", 37017, 0, 0);
 
-			network::packet sent_packet; 
-			 
-			sent_packet.write(std::string("hello there"));
+	RakNet::Packet *packet;
 
-			auto result = udp_socket.send(network::ip(27017, network::ip::get_local_ipv4()), sent_packet);
-			  
-			std::cout << "\nResult: " << result.result << "Bytes: " << result.bytes_transferred << std::endl;
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+			{
+				printf("Our connection request has been accepted.\n");
+
+				// Use a BitStream to write a custom user message
+				// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
+				RakNet::BitStream bsOut;
+				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
+				bsOut.Write("Hello world");
+				peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
+			}
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("A client has disconnected.\n");
+				break;
+			case ID_CONNECTION_LOST:
+				printf("A client lost the connection.\n");
+				break;
+
+			case ID_GAME_MESSAGE_1:
+			{
+				RakNet::RakString rs;
+				RakNet::BitStream bsIn(packet->data, packet->length, false);
+				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+				bsIn.Read(rs);
+				printf("%s\n", rs.C_String());
+			}
+				break;
+
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
 		}
-	} 
+	}
 
-	lua_state.dofile("init.lua"); 
+
+	lua_state.dofile("init.lua");
 
 	framework::deinit();
 	return 0;
-}  
+}
