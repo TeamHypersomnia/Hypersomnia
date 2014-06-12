54018252b95fcc869fec884349512ec203614473
geneotech <unknownunreleased@gmail.com>
2014-06-12 02:01:24 +0200

    successfully recreated client in lua

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #pragma once
-#include "RakPeerInterface.h"
-#include "MessageIdentifiers.h"
-#include "BitStream.h"
-#include "RakNetTypes.h"
-
 #include "game_framework/resources/lua_state_wrapper.h"
 #include "game_framework/game_framework.h"
-
-
-enum GameMessages
-{
-	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
-};
- 
 
 int main() {
 	framework::init();
@@ -21,60 +9,6 @@ int main() {
 	lua_state.bind_whole_engine();
 
 	lua_state.dofile("init.lua");
-
-	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
-
-	peer->Startup(1, &RakNet::SocketDescriptor(), 1);
-	auto res = peer->Connect("127.0.0.1", 37017, 0, 0);
-
-	RakNet::Packet *packet;
-
-	while (1) 
-	{
-		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
-		{
-			switch (packet->data[0])
-			{
-			case ID_CONNECTION_REQUEST_ACCEPTED:
-			{
-				printf("Our connection request has been accepted.\n");
-
-				// Use a BitStream to write a custom user message
-				// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
-				RakNet::BitStream bsOut;
-				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
-				bsOut.Write("Hello world");
-				peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
-			}
-				break;
-			case ID_NO_FREE_INCOMING_CONNECTIONS:
-				printf("The server is full.\n");
-				break;
-			case ID_DISCONNECTION_NOTIFICATION:
-				printf("A client has disconnected.\n");
-				break;
-			case ID_CONNECTION_LOST:
-				printf("A client lost the connection.\n");
-				break;
-
-			case ID_GAME_MESSAGE_1:
-			{
-				RakNet::RakString rs;
-				RakNet::BitStream bsIn(packet->data, packet->length, false);
-				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-				bsIn.Read(rs);
-				printf("Game message received: %s\n", rs.C_String());
-			}
-				break;
-
-			default:
-				printf("Message with identifier %i has arrived.\n", packet->data[0]);
-				break;
-			}
-		}
-	}
-
-
 
 	framework::deinit();
 	return 0;
--- output/init.lua
+++ output/init.lua
@@ -1,7 +1,43 @@
 dofile "config.lua"
 
 ENGINE_DIRECTORY = "..\\..\\Augmentations\\scripts\\"
-dofile "..\\..\\Augmentations\\scripts\\load_libraries.lua"
+dofile (ENGINE_DIRECTORY .. "load_libraries.lua")
+
+client = network_interface()
+client:connect("127.0.0.1", 37017)
+
+received = network_packet()
+
+network_message.ID_GAME_MESSAGE_1 = network_message.ID_USER_PACKET_ENUM + 1
+
+while true do
+	if (client:receive(received)) then
+		local message_type = received:byte(0)
+	
+		if message_type == network_message.ID_CONNECTION_REQUEST_ACCEPTED then
+			print("Our connection request has been accepted.");
+		
+			bsOut = BitStream()
+			bsOut:WriteByte(UnsignedChar(network_message.ID_GAME_MESSAGE_1))
+			WriteCString(bsOut, "Hello world")
+			client:send(bsOut, send_priority.HIGH_PRIORITY, send_reliability.RELIABLE_ORDERED, 0, received:guid(), false)
+		elseif message_type == network_message.ID_NO_FREE_INCOMING_CONNECTIONS then
+			print("The server is full.\n")
+		elseif message_type == network_message.ID_DISCONNECTION_NOTIFICATION then
+			print("A client has disconnected.\n")
+		elseif message_type == network_message.ID_CONNECTION_LOST then
+			print("A client lost the connection.\n")
+		elseif message_type == network_message.ID_GAME_MESSAGE_1 then
+			rs = RakString()
+			bsIn = received:get_bitstream()
+			bsIn:IgnoreBytes(1)
+			bsIn:ReadRakString(rs)
+			print("Game message received: " .. rs:C_String())
+		else
+			print("Message with identifier " .. message_type .. " has arrived.")
+		end	
+	end
+end
 
 -- enter the game
 dofile "hypersomnia\\scripts\\start.lua"
