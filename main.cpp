6c686dbea1fe31e05abd6a9088670f64a276d0d4
geneotech <unknownunreleased@gmail.com>
2014-06-20 16:54:10 +0200

    corrected main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #pragma once
-#include "game_framework/resources/lua_state_wrapper.h"
+#include "utilities/lua_state_wrapper.h"
 #include "game_framework/game_framework.h"
 
 int main() {
 	framework::init();
 
-	resources::lua_state_wrapper lua_state;
-	lua_state.bind_whole_engine();
+	augs::lua_state_wrapper lua_state;
+	framework::bind_whole_engine(lua_state);
 
 	lua_state.dofile("init.lua");
 
