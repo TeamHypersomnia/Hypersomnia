1b323dfe14524ba90596163751ea3f72f4a2cf3d
geneotech <unknownunreleased@gmail.com>
2014-06-25 02:04:32 +0200

    more unit tests passed

--- utilities/network/net_channel.cpp
+++ utilities/network/net_channel.cpp
@@ -21,11 +21,16 @@ namespace augs {
 		}
 
 		void reliable_channel_sender::write_data(RakNet::BitStream& output) {
-			for (size_t i = 0; i < reliable_buf.size(); ++i) 
-				if (reliable_buf[i].flag_for_deletion)
-					for (auto& iter : sequence_to_reliable_range)
-						if (i < iter.second) 
-							--iter.second;
+			for (auto& iter : sequence_to_reliable_range) {
+				auto new_value = iter.second;
+
+				for (size_t i = 0; i < reliable_buf.size(); ++i) {
+					if (reliable_buf[i].flag_for_deletion && i < iter.second)
+						--new_value;
+				}
+
+				iter.second = new_value;
+			}
 
 			reliable_buf.erase(std::remove_if(reliable_buf.begin(), reliable_buf.end(), [](const message& m){ return m.flag_for_deletion; }), reliable_buf.end());
 
@@ -67,7 +72,7 @@ namespace augs {
 			input.Read(update_to_sequence);
 			input.Read(update_from_sequence);
 
-			if (update_from_sequence == last_sequence) {
+			if (sequence_more_recent(update_to_sequence, last_sequence) && update_from_sequence == last_sequence) {
 				last_sequence = update_to_sequence;
 				return true;
 			}
@@ -85,10 +90,10 @@ namespace augs {
 
 #include <gtest\gtest.h>
 
-TEST(NetChannel, SingleTransmissionDeleteAllPending) {
-	using namespace augs;
-	using namespace network;
+using namespace augs;
+using namespace network;
 
+TEST(NetChannel, SingleTransmissionDeleteAllPending) {
 	reliable_channel_sender sender;
 	reliable_channel_receiver receiver;
 
@@ -126,6 +131,285 @@ TEST(NetChannel, SingleTransmissionDeleteAllPending) {
 	EXPECT_EQ(1, receiver.last_sequence);
 }
 
-TEST(NetChannel, PartialUpdates) {
+TEST(NetChannel, PastAcknowledgementDeletesSeveralPending) {
+	reliable_channel_sender sender;
+	reliable_channel_receiver receiver;
+
+	RakNet::BitStream bs[15];
+	reliable_channel_sender::message msg[15];
+
+	for (int i = 0; i < 15; ++i) {
+		bs[i].Write(int(i));
+		msg[i].output_bitstream = &bs[i];
+	}
+
+	RakNet::BitStream sender_packets[15];
+	RakNet::BitStream receiver_packet;
+
+	/* post four messages */
+	sender.post_message(msg[0]);
+	sender.post_message(msg[1]);
+	sender.post_message(msg[2]);
+	sender.post_message(msg[3]);
+
+	sender.write_data(sender_packets[0]);
+
+	sender.post_message(msg[4]);
+	sender.post_message(msg[5]);
+
+	sender.write_data(sender_packets[1]);
+
+	sender.post_message(msg[6]);
+	sender.post_message(msg[7]);
+	sender.post_message(msg[8]);
+
+	sender.write_data(sender_packets[2]);
+
+	receiver.read_sequence(sender_packets[0]);
+	receiver.write_ack(receiver_packet);
+
+	sender.read_ack(receiver_packet);
+
+	EXPECT_EQ(3, sender.sequence);
+	EXPECT_EQ(5, sender.reliable_buf.size());
+	EXPECT_EQ(1, sender.ack_sequence);
+
+	EXPECT_EQ(1, receiver.last_sequence);
+}
+
+TEST(NetChannel, FlagForDeletionAndAck) {
+	reliable_channel_sender sender;
+	reliable_channel_receiver receiver;
+
+	RakNet::BitStream bs[15];
+	reliable_channel_sender::message msg[15];
+
+	for (int i = 0; i < 15; ++i) {
+		bs[i].Write(int(i));
+		msg[i].output_bitstream = &bs[i];
+	}
+
+	RakNet::BitStream sender_packets[15];
+	RakNet::BitStream receiver_packet;
+
+	/* post four messages */
+	sender.post_message(msg[0]);
+	sender.post_message(msg[1]);
+	sender.post_message(msg[2]);
+	sender.post_message(msg[3]);
+
+	sender.write_data(sender_packets[0]);
+
+	sender.post_message(msg[4]);
+	sender.post_message(msg[5]);
+	sender.post_message(msg[6]);
+
+	sender.write_data(sender_packets[1]);
+	
+	sender.reliable_buf[0].flag_for_deletion = true;
+	sender.reliable_buf[2].flag_for_deletion = true;
+	sender.reliable_buf[4].flag_for_deletion = true;
+	sender.reliable_buf[5].flag_for_deletion = true;
+	sender.reliable_buf[6].flag_for_deletion = true;
+
+	sender.post_message(msg[7]);
+	sender.post_message(msg[8]);
+
+	sender.write_data(sender_packets[2]);
+	sender.write_data(sender_packets[3]);
+	sender.write_data(sender_packets[4]);
+	sender.write_data(sender_packets[5]);
+
+	receiver.read_sequence(sender_packets[0]);
+	int table[4];
+	sender_packets[0].Read(table[0]);
+	sender_packets[0].Read(table[1]);
+	sender_packets[0].Read(table[2]);
+	sender_packets[0].Read(table[3]);
+
+	EXPECT_EQ(0, table[0]);
+	EXPECT_EQ(1, table[1]);
+	EXPECT_EQ(2, table[2]);
+	EXPECT_EQ(3, table[3]);
+
+
+	receiver.write_ack(receiver_packet);
+
+	sender.read_ack(receiver_packet);
+
+	EXPECT_EQ(6, sender.sequence);
+	EXPECT_EQ(1, sender.ack_sequence);
+	EXPECT_EQ(2, sender.reliable_buf.size());
+
+
+	EXPECT_EQ(bs+7, sender.reliable_buf[0].output_bitstream);
+	EXPECT_EQ(bs+8, sender.reliable_buf[1].output_bitstream);
+
+	EXPECT_EQ(1, receiver.last_sequence);
+}
+
+
+
+TEST(NetChannel, SequenceNumberOverflowMultipleTries) {
+
+	reliable_channel_sender sender;
+	reliable_channel_receiver receiver;
+
+	sender.sequence = std::numeric_limits<unsigned short>::max();
+	sender.ack_sequence = std::numeric_limits<unsigned short>::max();
+
+	receiver.last_sequence = std::numeric_limits<unsigned short>::max();
+
+	for (int k = 0; k < 10; ++k) {
+		RakNet::BitStream bs[15];
+		reliable_channel_sender::message msg[15];
+
+		for (int i = 0; i < 15; ++i) {
+			bs[i].Write(int(i));
+			msg[i].output_bitstream = &bs[i];
+		}
+
+		RakNet::BitStream sender_packets[15];
+		RakNet::BitStream receiver_packet;
+
+		/* post four messages */
+		sender.post_message(msg[0]);
+		sender.post_message(msg[1]);
+		sender.post_message(msg[2]);
+		sender.post_message(msg[3]);
+
+		sender.write_data(sender_packets[0]);
+
+		sender.post_message(msg[4]);
+		sender.post_message(msg[5]);
+		sender.post_message(msg[6]);
+
+		sender.write_data(sender_packets[1]);
+
+		sender.reliable_buf[0].flag_for_deletion = true;
+		sender.reliable_buf[2].flag_for_deletion = true;
+		sender.reliable_buf[4].flag_for_deletion = true;
+		sender.reliable_buf[5].flag_for_deletion = true;
+		sender.reliable_buf[6].flag_for_deletion = true;
+
+		sender.post_message(msg[7]);
+		sender.post_message(msg[8]);
+
+		sender.write_data(sender_packets[2]);
+		sender.write_data(sender_packets[3]);
+		sender.write_data(sender_packets[4]);
+		sender.write_data(sender_packets[5]);
+
+		receiver.read_sequence(sender_packets[0]);
+		int table[4];
+		sender_packets[0].Read(table[0]);
+		sender_packets[0].Read(table[1]);
+		sender_packets[0].Read(table[2]);
+		sender_packets[0].Read(table[3]);
+
+		EXPECT_EQ(0, table[0]);
+		EXPECT_EQ(1, table[1]);
+		EXPECT_EQ(2, table[2]);
+		EXPECT_EQ(3, table[3]);
+
+		receiver.write_ack(receiver_packet);
+
+		sender.read_ack(receiver_packet);
+
+		if (k == 0) {
+			EXPECT_EQ(5, sender.sequence);
+			EXPECT_EQ(0, sender.ack_sequence);
+			EXPECT_EQ(0, receiver.last_sequence);
+		}
+
+		EXPECT_EQ(2, sender.reliable_buf.size());
+
+		EXPECT_EQ(bs + 7, sender.reliable_buf[0].output_bitstream);
+		EXPECT_EQ(bs + 8, sender.reliable_buf[1].output_bitstream);
+
+		sender.reliable_buf.clear();
+	}
+}
+
+
+TEST(NetChannel, OutOfDatePackets) {
+	reliable_channel_sender sender;
+	reliable_channel_receiver receiver;
+
+	sender.sequence = std::numeric_limits<unsigned short>::max();
+	sender.ack_sequence = std::numeric_limits<unsigned short>::max();
+
+	receiver.last_sequence = std::numeric_limits<unsigned short>::max();
+
+	for (int k = 0; k < 10; ++k) {
+		RakNet::BitStream bs[15];
+		reliable_channel_sender::message msg[15];
+
+		for (int i = 0; i < 15; ++i) {
+			bs[i].Write(int(i));
+			msg[i].output_bitstream = &bs[i];
+		}
+
+		RakNet::BitStream sender_packets[15];
+		RakNet::BitStream receiver_packet;
+
+		/* post four messages */
+		sender.post_message(msg[0]);
+		sender.post_message(msg[1]);
+		sender.post_message(msg[2]);
+		sender.post_message(msg[3]);
+
+		sender.write_data(sender_packets[0]);
+
+		sender.post_message(msg[4]);
+		sender.post_message(msg[5]);
+		sender.post_message(msg[6]);
+
+		sender.write_data(sender_packets[1]);
+
+		sender.reliable_buf[0].flag_for_deletion = true;
+		sender.reliable_buf[2].flag_for_deletion = true;
+		sender.reliable_buf[4].flag_for_deletion = true;
+		sender.reliable_buf[5].flag_for_deletion = true;
+		sender.reliable_buf[6].flag_for_deletion = true;
+
+		sender.post_message(msg[7]);
+		sender.post_message(msg[8]);
+
+		sender.write_data(sender_packets[2]);
+		sender.write_data(sender_packets[3]);
+		sender.write_data(sender_packets[4]);
+		sender.write_data(sender_packets[5]);
+
+		receiver.read_sequence(sender_packets[1]);
+		receiver.read_sequence(sender_packets[0]);
+		int table[4];
+		sender_packets[0].Read(table[0]);
+		sender_packets[0].Read(table[1]);
+		sender_packets[0].Read(table[2]);
+		sender_packets[0].Read(table[3]);
+
+		EXPECT_EQ(0, table[0]);
+		EXPECT_EQ(1, table[1]);
+		EXPECT_EQ(2, table[2]);
+		EXPECT_EQ(3, table[3]);
+
+		receiver.write_ack(receiver_packet);
+
+		sender.read_ack(receiver_packet);
+
+		if (k == 0) {
+			EXPECT_EQ(5, sender.sequence);
+			EXPECT_EQ(1, sender.ack_sequence);
+			EXPECT_EQ(1, receiver.last_sequence);
+		}
+
+		EXPECT_EQ(2, sender.reliable_buf.size());
+
+		EXPECT_EQ(bs + 7, sender.reliable_buf[0].output_bitstream);
+		EXPECT_EQ(bs + 8, sender.reliable_buf[1].output_bitstream);
+
+		sender.reliable_buf.clear();
+	}
 
 }
