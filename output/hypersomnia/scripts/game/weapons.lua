218d51d04e86237257e7011db58b15fc121002b0
geneotech <unknownunreleased@gmail.com>
2014-07-16 03:06:33 +0200

    optionally skip render components

--- output/hypersomnia/scripts/game/weapons.lua
+++ output/hypersomnia/scripts/game/weapons.lua
@@ -1,32 +1,27 @@
-function create_weapons(scene)
+function create_weapons(scene, include_render)
 	local weapons = {}
 	
 	weapons.m4a1 = {
 		current_rounds = 300,
 		is_automatic = true,
 		bullets_once = 1,
-		bullet_speed = minmax(2000, 2000),
+		bullet_speed = minmax(5000, 5000),
 		
 		shooting_interval_ms = 100,
 		spread_degrees = 0,
 		shake_radius = 9.5,
 		shake_spread_degrees = 45,
 		
-		bullet_distance_offset = vec2(50, 0),
+		bullet_barrel_offset = vec2(50, 0),
 		
 		bullet_entity = {
-			render = {
-				model = scene.bullet_sprite,
-				layer = render_layers.BULLETS
-			},
-			
 			physics = {
 				body_type = Box2D.b2_dynamicBody,
 	
 				body_info = {
 					filter = filter_bullets,
 					shape_type = physics_info.RECT,
-					rect_size = scene.bullet_sprite.size,
+					rect_size = vec2(35, 19*0.5),
 					fixed_rotation = true,
 					density = 0.1,
 					bullet = true
@@ -36,6 +31,13 @@ function create_weapons(scene)
 		
 		max_bullet_distance = 2000	
 	}
+	
+	if include_render == true then
+		weapons.m4a1.bullet_entity.render = {
+			model = scene.bullet_sprite,
+			layer = render_layers.BULLETS
+		}
+	end
 	
 	scene.weapons = weapons
 end
