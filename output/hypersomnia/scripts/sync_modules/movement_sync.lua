7b50595a47f691b9090dacb9118d8fbc9646da6b
geneotech <unknownunreleased@gmail.com>
2014-08-06 23:30:48 +0200

    bugfix

--- output/hypersomnia/scripts/sync_modules/movement_sync.lua
+++ output/hypersomnia/scripts/sync_modules/movement_sync.lua
@@ -1,4 +1,4 @@
 protocol.replication_tables.movement = create_replication_table {
-	"b2Vec2", "position", function (object) return b2Vec2(object.cpp_entity.physics.body:GetPosition()) end,
-	"b2Vec2", "velocity", function (object) return b2Vec2(object.cpp_entity.physics.body:GetLinearVelocity()) end
+	"b2Vec2", "position", function (object) if object.cpp_entity.physics ~= nil then return b2Vec2(object.cpp_entity.physics.body:GetPosition()) else return b2Vec2(0, 0) end end,
+	"b2Vec2", "velocity", function (object) if object.cpp_entity.physics ~= nil then return b2Vec2(object.cpp_entity.physics.body:GetLinearVelocity()) else return b2Vec2(0, 0) end end
 }
