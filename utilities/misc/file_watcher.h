642d9f8624e24ae4c85b62bbc3ab7ec9b8f921de
geneotech <unknown.unreleased@gmail.com>
2015-10-19 16:22:38 +0200

    better file_watcher implementation

--- utilities/misc/file_watcher.h
+++ utilities/misc/file_watcher.h
@@ -1,19 +1,39 @@
 #pragma once
 #define UNICODE
-#include <vector>
-#include "readdir/ReadDirectoryChanges.h"
 #include "vector_wrapper.h"
 
+#include "FileWatcher/FileWatcher.h"
 #include "timer.h"
+#include <functional>
+#include <mutex>
 
 namespace augs {
 	namespace misc {
 		class file_watcher {
+			struct update_listener : public FW::FileWatchListener {
+				std::function<void(std::string)> callback;
+				void handleFileAction(FW::WatchID watchid, const FW::String& dir, const FW::String& filename, FW::Action action) { callback( filename ); }
+			};
+
+			std::vector<std::string> filenames;
+			std::mutex lock;
+
 			timer notification_interval;
-			CReadDirectoryChanges changes;
+			FW::FileWatcher fw;
+			
+			update_listener listener;
+
+
 		public:
+			file_watcher() {
+				listener.callback = [this](std::string filename) {
+					std::unique_lock<std::mutex> lock(lock);
+					filenames.push_back(filename);
+				};
+			}
+
 			misc::vector_wrapper<std::string> get_modified_files();
-			void add_directory(const std::wstring& directory, bool subtree);
+			void add_directory(const std::string& directory);
 		};
 	}
 }
