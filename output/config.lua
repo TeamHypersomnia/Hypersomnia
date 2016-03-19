f18fe5cfce8a5824d3e0ed97d7c2b64f219955d6
geneotech <unknown.unreleased@gmail.com>
2016-03-19 18:18:17 +0100

    cursor always invisible

--- output/config.lua
+++ output/config.lua
@@ -27,11 +27,12 @@ config_table = {
 	jitter = 0
 }
 
+set_cursor_visible(0)
+
 if config_table.fullscreen == 1 then
 	config_table.resolution_w = get_display().w
 	config_table.resolution_h = get_display().h
 	set_display(config_table.resolution_w, config_table.resolution_h, 32)
-	set_cursor_visible(0)
 end
 
 local borders_type = glwindow.ALL_WINDOW_ELEMENTS
