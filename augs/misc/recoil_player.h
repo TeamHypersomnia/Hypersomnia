544f4ee748450e797368b86b8a96c5d90b6db5cb
Kartezjan <endless.journey0@gmail.com>
2016-04-15 17:03:25 +0200

    Update recoil_player.h

--- augs/misc/recoil_player.h
+++ augs/misc/recoil_player.h
@@ -9,6 +9,7 @@ public:
 	int current_offset = 0;
 	bool reversed = false;
 	int delta_offset = 0;
+	int delta_offset_maximum = 0;
 	// augs::deterministic_timestamp since_last_shot;
 
 	double single_cooldown_duration_ms = 50.0;
