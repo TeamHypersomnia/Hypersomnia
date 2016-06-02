5fad6e1db4729e87efe357c5c786528f4cad0111
geneotech <unknown.unreleased@gmail.com>
2016-06-02 20:43:10 +0200

    uniquify utility

--- augs/stlutil.h
+++ augs/stlutil.h
@@ -19,3 +19,35 @@ template<class T, class L>
 void erase_remove(std::vector<T>& v, const L& l) {
 	v.erase(std::remove_if(v.begin(), v.end(), l), v.end());
 }
+
+struct target_less
+{
+    template<class It>
+    bool operator()(It const &a, It const &b) const { return *a < *b; }
+};
+struct target_equal
+{
+    template<class It>
+    bool operator()(It const &a, It const &b) const { return *a == *b; }
+};
+template<class It> It uniquify(It begin, It const end)
+{
+    std::vector<It> v;
+    v.reserve(static_cast<size_t>(std::distance(begin, end)));
+    for (It i = begin; i != end; ++i)
+    { v.push_back(i); }
+    std::sort(v.begin(), v.end(), target_less());
+    v.erase(std::unique(v.begin(), v.end(), target_equal()), v.end());
+    std::sort(v.begin(), v.end());
+    size_t j = 0;
+    for (It i = begin; i != end && j != v.size(); ++i)
+    {
+        if (i == v[j])
+        {
+            using std::iter_swap; iter_swap(i, begin);
+            ++j;
+            ++begin;
+        }
+    }
+    return begin;
+}
