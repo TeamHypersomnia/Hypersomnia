647643d82f4df432f79f105a1446477556740a5c
geneotech <unknown.unreleased@gmail.com>
2016-06-02 21:37:06 +0200

    compilation fix

--- client/draw_tile_highlights.cpp
+++ client/draw_tile_highlights.cpp
@@ -8,17 +8,17 @@ void draw_tile_highlights(
 	shared::state_for_drawing_renderable& in) {
 }
 
-vec2i get_random_coordinate_on_a_special_tile(
-	components::tile_layer& tile_layer,
-	augs::vector_wrapper<int>& shining,
-	shared::state_for_drawing_renderable& in) {
-
-	auto visible_tiles = tile_layer.get_visible_tiles(in);
-	tile_layer.generate_indices_by_type(visible_tiles);
-
-	int random_type = shining.at(randval(int(0), int(shining.size() - 1)));
-	if (int(tile_layer.indices_by_type.size()) - 1 < random_type) return vec2i(-1, -1);
-	if (tile_layer.indices_by_type[random_type].empty()) return vec2i(-1, -1);
-	
-	return tile_layer.indices_by_type[random_type][randval(int(0), int(tile_layer.indices_by_type[random_type].size() - 1))];
-}
+//vec2i get_random_coordinate_on_a_special_tile(
+//	components::tile_layer& tile_layer,
+//	augs::vector_wrapper<int>& shining,
+//	shared::state_for_drawing_renderable& in) {
+//
+//	auto visible_tiles = tile_layer.get_visible_tiles(in);
+//	tile_layer.generate_indices_by_type(visible_tiles);
+//
+//	int random_type = shining.at(randval(int(0), int(shining.size() - 1)));
+//	if (int(tile_layer.indices_by_type.size()) - 1 < random_type) return vec2i(-1, -1);
+//	if (tile_layer.indices_by_type[random_type].empty()) return vec2i(-1, -1);
+//	
+//	return tile_layer.indices_by_type[random_type][randval(int(0), int(tile_layer.indices_by_type[random_type].size() - 1))];
+//}
