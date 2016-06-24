2dfd28e058bedafd1aa75beaa93d54392d66da33
geneotech <unknown.unreleased@gmail.com>
2016-06-24 21:00:22 +0200

    missing files

---
+++ augs/misc/stepped_timing.cpp
@@ -0,0 +1,47 @@
+#include "stepped_timing.h"
+#include "delta.h"
+
+namespace augs {
+	stepped_timestamp stepped_timestamp::operator-(stepped_timestamp b) const {
+		stepped_timestamp stamp;
+		stamp.step = step - b.step;
+		return stamp;
+	}
+
+	double stepped_timestamp::in_seconds(fixed_delta delta) const {
+		return step * delta.in_seconds();
+	}
+
+	double stepped_timestamp::in_milliseconds(fixed_delta delta) const {
+		return step * delta.in_milliseconds();
+	}
+
+	stepped_cooldown::stepped_cooldown(float cooldown_duration_ms) : ready_to_fire(true), cooldown_duration_ms(cooldown_duration_ms) {}
+
+	bool stepped_cooldown::is_ready(fixed_delta t) const {
+		return ready_to_fire || (t.get_timestamp() - when_last_fired).in_milliseconds(t) > cooldown_duration_ms;
+	}
+
+	bool stepped_cooldown::try_to_fire_and_reset(fixed_delta t) {
+		if (is_ready(t)) {
+			ready_to_fire = false;
+			when_last_fired = t.get_timestamp();
+			return true;
+		}
+
+		return false;
+	}
+
+	void stepped_timeout::set(float duration_ms, fixed_delta t) {
+		timeout_duration_ms = duration_ms;
+		when_started = t.get_timestamp();
+	}
+
+	bool stepped_timeout::passed(fixed_delta t) const {
+		return !is_set || (t.get_timestamp() - when_started).in_milliseconds(t) > timeout_duration_ms;
+	}
+
+	bool stepped_timeout::lasts(fixed_delta t) const {
+		return !passed(t);
+	}
+}
---
+++ augs/misc/stepped_timing.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "ensure.h"
+
+namespace augs {
+	class fixed_delta;
+
+	struct stepped_timestamp {
+		unsigned long long step = 0;
+
+		stepped_timestamp operator-(stepped_timestamp b) const;
+
+		double in_seconds(fixed_delta) const;
+		double in_milliseconds(fixed_delta) const;
+	};
+
+	struct stepped_timeout {
+		stepped_timestamp when_started;
+
+		bool is_set = false;
+		float timeout_duration_ms = 1000.f;
+
+		void set(float timeout_duration_ms, fixed_delta);
+		bool passed(fixed_delta) const;
+		bool lasts(fixed_delta) const;
+	};
+
+	struct stepped_cooldown {
+		stepped_timestamp when_last_fired;
+
+		bool ready_to_fire = true;
+		float cooldown_duration_ms = 1000.f;
+
+		stepped_cooldown(float cooldown_duration_ms);
+
+		bool is_ready(fixed_delta t) const;
+		bool try_to_fire_and_reset(fixed_delta t);
+	};
+
+}
