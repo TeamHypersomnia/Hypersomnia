6ac7989fd983012c86e95588c94c7284ed51806a
geneotech <unknown.unreleased@gmail.com>
2016-07-17 10:01:23 +0200

    rewritten directory.cp to experimental and removed dirent

--- augs/filesystem/directory.cpp
+++ augs/filesystem/directory.cpp
@@ -1,35 +1,26 @@
 #include "directory.h"
-#include "dirent.h"
-#include <direct.h>
 #include "ensure.h"
+#include <experimental/filesystem>
+#include <string>
+
+namespace fs = std::experimental::filesystem;
 
 namespace augs {
 	void create_directory(std::wstring filename) {
-		_wmkdir(filename.c_str());
+		fs::create_directory(filename);
 	}
 
 	void create_directory(std::string filename) {
-		_mkdir(filename.c_str());
+		fs::create_directory(filename);
 	}
 
 	std::vector<std::string> get_all_files_in_directory(std::string dirpath) {
-		std::vector<std::string> out;
+		std::vector<std::string> result;
 
-		std::string dirpaths(dirpath.begin(), dirpath.end());
+		for (fs::recursive_directory_iterator i("."), end; i != end; ++i)
+			if (!is_directory(i->path()))
+				result.push_back(i->path().filename().generic_string());
 
-		DIR *dir;
-		struct dirent *ent;
-		if ((dir = opendir(dirpaths.c_str())) != NULL) {
-			/* print all the files and directories within directory */
-			while ((ent = readdir(dir)) != NULL) {
-				std::string d_name(ent->d_name);
-				out.push_back(d_name);
-			}
-			closedir(dir);
-		}
-		else
-			ensure(0);
-
-		return out;
+		return result;
 	}
 }
