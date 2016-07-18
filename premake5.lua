209148efad027914c708891c9d8c43c0be8cd5bb
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-07-19 00:40:14 +0200

    Introduced Premake.

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+workspace "Hypersomnia"
+  configurations { "Debug", "Release" }
+  platforms { "Linux", "Win64" }
+
+project "Augmentations"
+  kind "SharedLib"
+  language "C++"
+  targetdir "bin/"
+
+  includedirs { ".", "augs/**", "3rdparty" }
+  files { "augs/**.h", "augs/**.cpp" }
+--  flags { "C++14" }
+  buildoptions { "-std=c++1z" }
+
+  filter "configurations:Debug"
+    defines { "DEBUG", "ENABLE_ENSURE" }
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+
+  filter { "platforms:Win64" }
+    system "Windows"
+    architecture "x64"
+    defines { "PLATFORM_WINDOWS" }
+
+  filter { "platforms:Linux" }
+    system "Linux"
+    architecture "x64"
+    defines { "PLATFORM_LINUX" }
+    includedirs { "/usr/include/freetype2", "/usr/include/freetype2/freetype" } -- should make it global (windows?)
+
+  filter { "platforms:Linux", "configurations:Debug" }
+    buildoptions { "-ferror-limit=1" }
