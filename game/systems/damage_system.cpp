c3101affbbc3cd4a02f60959f7f4754b206b1a8b
geneotech <unknown.unreleased@gmail.com>
2016-07-21 22:07:57 +0200

    refactored damage system and temporarily disabled offset setting (the machete doesnt do damage because of constant resubstantialization)

--- game/systems/damage_system.cpp
+++ game/systems/damage_system.cpp
@@ -34,42 +34,44 @@ void damage_system::destroy_colliding_bullets_and_send_damage(fixed_step& step) 
 		auto subject_handle = cosmos[it.subject];
 		auto collider_handle = cosmos[it.collider];
 
-		auto* damage = collider_handle.find<components::damage>();
+		if (collider_handle.has<components::damage>()) {
+			auto& damage = collider_handle.get<components::damage>();
+			auto sender = cosmos[damage.sender];
 
-		bool bullet_colliding_with_sender =
-			damage && cosmos.get_handle(damage->sender).get_owner_body() == subject_handle.get_owner_body();
+			bool bullet_colliding_with_sender = sender.get_owner_body() == subject_handle.get_owner_body();
 
-		if (!bullet_colliding_with_sender && damage && damage->damage_upon_collision && damage->damage_charges_before_destruction > 0) {
-			auto& subject_of_impact = subject_handle.get_owner_body().get<components::physics>();
-			
-			vec2 impact_velocity = damage->custom_impact_velocity;
-			
-			if(impact_velocity.is_zero())
-				impact_velocity = velocity(collider_handle);
+			if (!bullet_colliding_with_sender && damage.damage_upon_collision && damage.damage_charges_before_destruction > 0) {
+				auto& subject_of_impact = subject_handle.get_owner_body().get<components::physics>();
 
-			if(damage->impulse_upon_hit > 0.f)
-				subject_of_impact.apply_force(vec2(impact_velocity).set_length(damage->impulse_upon_hit), it.point - subject_of_impact.get_mass_position());
+				vec2 impact_velocity = damage.custom_impact_velocity;
 
-			messages::damage_message damage_msg;
-			damage_msg.inflictor = it.collider;
-			damage_msg.subject = it.subject;
-			damage_msg.amount = damage->amount;
-			damage_msg.impact_velocity = impact_velocity;
-			damage_msg.point_of_impact = it.point;
-			step.messages.post(damage_msg);
+				if (impact_velocity.is_zero())
+					impact_velocity = velocity(collider_handle);
 
-			damage->saved_point_of_impact_before_death = it.point;
+				if (damage.impulse_upon_hit > 0.f)
+					subject_of_impact.apply_force(vec2(impact_velocity).set_length(damage.impulse_upon_hit), it.point - subject_of_impact.get_mass_position());
 
-			auto owning_capability = subject_handle.get_owning_transfer_capability();
+				messages::damage_message damage_msg;
+				damage_msg.inflictor = it.collider;
+				damage_msg.subject = it.subject;
+				damage_msg.amount = damage.amount;
+				damage_msg.impact_velocity = impact_velocity;
+				damage_msg.point_of_impact = it.point;
+				step.messages.post(damage_msg);
 
-			bool is_victim_a_held_item = cosmos[owning_capability].alive() && owning_capability != it.subject;
+				damage.saved_point_of_impact_before_death = it.point;
 
-			if (!is_victim_a_held_item && damage->destroy_upon_damage) {
-				damage->damage_charges_before_destruction--;
-				
-				// delete only once
-				if(damage->damage_charges_before_destruction == 0)
-					step.messages.post(messages::queue_destruction(it.collider));
+				auto owning_capability = subject_handle.get_owning_transfer_capability();
+
+				bool is_victim_a_held_item = owning_capability.alive() && owning_capability != it.subject;
+
+				if (!is_victim_a_held_item && damage.destroy_upon_damage) {
+					damage.damage_charges_before_destruction--;
+
+					// delete only once
+					if (damage.damage_charges_before_destruction == 0)
+						step.messages.post(messages::queue_destruction(it.collider));
+				}
 			}
 		}
 	}
