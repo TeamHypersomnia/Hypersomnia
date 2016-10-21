c2e59b86daf163ba807136440946a06e2f918bc9
geneotech <unknown.unreleased@gmail.com>
2016-10-21 04:28:43 +0200

    config fullscreen fix

--- output/config.lua
+++ output/config.lua
@@ -68,6 +68,8 @@ if config_table.debug_disable_cursor_clipping == 0 then
 end
 
 if config_table.fullscreen == 1 then
+	config_table.window_x = 0
+	config_table.window_y = 0
 	config_table.resolution_w = get_display().w
 	config_table.resolution_h = get_display().h
 	set_display(config_table.resolution_w, config_table.resolution_h, 32)
