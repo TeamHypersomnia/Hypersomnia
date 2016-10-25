60aa9848dc46b7da8091fd3f953084058db13087
geneotech <unknown.unreleased@gmail.com>
2016-10-26 00:13:25 +0200

    extracted trivial_pair template from minmax

--- augs/misc/minmax.h
+++ augs/misc/minmax.h
@@ -1,30 +1,7 @@
 #pragma once
-#include <utility>
+#include "trivial_pair.h"
 
 namespace augs {
 	template<class T>
-	class minmax {
-	public:
-		typedef std::pair<T, T> pair;
-
-		T first, second;
-
-		minmax(const T& a = T(), const T& b = T()) : first(a), second(b) {}
-		minmax(const pair& p) : first(p.first), second(p.second) {}
-
-		minmax& operator=(const pair& p) {
-			first = p.first;
-			second = p.second;
-			return *this;
-		}
-
-		void set(const T& a, const T& b) {
-			first = a;
-			second = b;
-		}
-
-		operator pair() {
-			return{ first, second };
-		}
-	};
+	using minmax = trivial_pair<T, T>;
 }
---
+++ augs/misc/trivial_pair.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <utility>
+
+namespace augs {
+	template<class A, class B>
+	class trivial_pair {
+	public:
+		typedef std::pair<A, B> pair;
+
+		A first;
+		B second;
+
+		trivial_pair(const A& a = A(), const B& b = B()) : first(a), second(b) {}
+		trivial_pair(const pair& p) : first(p.first), second(p.second) {}
+
+		trivial_pair& operator=(const pair& p) {
+			first = p.first;
+			second = p.second;
+			return *this;
+		}
+
+		void set(const A& a, const B& b) {
+			first = a;
+			second = b;
+		}
+
+		operator pair() {
+			return{ first, second };
+		}
+
+		bool operator<(const trivial_pair& b) const {
+			return pair(*this) < pair(b);
+		}
+
+		bool operator==(const trivial_pair& b) const {
+			return pair(*this) == pair(b);
+		}
+	};
+}
