6395b47b809e3e778618482e685d5f109834533c
geneotech <unknown.unreleased@gmail.com>
2016-10-26 20:10:02 +0200

    extracted gui_flags.h

---
+++ augs/gui/gui_flags.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace augs {
+	namespace gui {
+		enum class flag {
+			DISABLE_HOVERING,
+			ENABLE_DRAWING,
+			FETCH_WHEEL,
+			PRESERVE_FOCUS,
+			FOCUSABLE,
+			ENABLE_DRAWING_OF_CHILDREN,
+			SNAP_SCROLL_TO_CONTENT_SIZE,
+			SCROLLABLE,
+			CLIP,
+
+			COUNT
+		};
+	}
+}
