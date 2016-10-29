918032ac47daaf97fc0a3b1f0fd1565197e548f3
geneotech <unknown.unreleased@gmail.com>
2016-10-29 21:12:36 +0200

    removed using namespace

--- game/messages/crosshair_intent_message.h
+++ game/messages/crosshair_intent_message.h
@@ -4,9 +4,6 @@
 
 #include "intent_message.h"
 
-/* everything is a state since for actions we can just ignore states with flag set to false */
-using namespace augs;
-
 namespace messages {
 	struct crosshair_intent_message : public intent_message {
 		vec2 crosshair_base_offset;
