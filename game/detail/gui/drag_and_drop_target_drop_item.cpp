3dda7e4aea859b311be583f04d77b07299ec1eeb
geneotech <unknown.unreleased@gmail.com>
2016-10-30 22:30:23 +0100

    untemplatized drag_and_drop_target_drop_item

--- game/detail/gui/drag_and_drop_target_drop_item.cpp
+++ game/detail/gui/drag_and_drop_target_drop_item.cpp
@@ -1,7 +1,47 @@
 #include "drag_and_drop_target_drop_item.h"
+#include "game/components/gui_element_component.h"
+#include "game/components/gui_element_component.h"
+#include "game/components/item_component.h"
+#include "game/detail/gui/root_of_inventory_gui.h"
+#include "game/detail/gui/gui_element_tree.h"
+#include "game/detail/inventory_slot.h"
+#include "game/detail/inventory_slot_handle.h"
+#include "game/transcendental/entity_handle.h"
+#include "game/transcendental/cosmos.h"
+#include "game/transcendental/step.h"
 
 drag_and_drop_target_drop_item::drag_and_drop_target_drop_item(const augs::gui::material new_mat) 
 	: mat(new_mat) {
 	unset_flag(augs::gui::flag::CLIP); 
 	unset_flag(augs::gui::flag::ENABLE_DRAWING);
+}
+
+void drag_and_drop_target_drop_item::draw(const const_dispatcher_context& context, const const_this_pointer& this_id, augs::gui::draw_info info) {
+	auto mat_coloured = this_id->mat;
+
+	if (this_id->detector.is_hovered)
+		mat_coloured.color.a = 255;
+	else
+		mat_coloured.color.a = 120;
+
+	draw_centered_texture(context, this_id, info, mat_coloured);
+}
+
+void drag_and_drop_target_drop_item::consume_gui_event(const dispatcher_context& context, const this_pointer& this_id, const augs::gui::event_info info) {
+	this_id->detector.update_appearance(info);
+}
+
+void drag_and_drop_target_drop_item::perform_logic_step(const dispatcher_context& context, const this_pointer& this_id, const fixed_delta& dt) {
+	auto& world = context.get_rect_world();
+	auto dragged_item = world.rect_held_by_lmb;
+
+	if (context.alive(dragged_item) && world.held_rect_is_dragged) {
+		this_id->set_flag(augs::gui::flag::ENABLE_DRAWING);
+	}
+	else {
+		this_id->unset_flag(augs::gui::flag::ENABLE_DRAWING);
+	}
+
+	this_id->rc.set_position(context.get_gui_element_component().get_initial_position_for(*this_id) - vec2(20, 20));
+	this_id->rc.set_size((*this_id->mat.tex).get_size() + vec2(40, 40));
 }
--- game/detail/gui/drag_and_drop_target_drop_item.h
+++ game/detail/gui/drag_and_drop_target_drop_item.h
@@ -3,10 +3,12 @@
 #include "augs/gui/material.h"
 #include "augs/gui/appearance_detector.h"
 
-#include "game/detail/gui/gui_element_location.h"
+#include "game/detail/gui/dispatcher_context.h"
 
 struct drag_and_drop_target_drop_item : game_gui_rect_node {
 	typedef drag_and_drop_target_drop_item_location location;
+	typedef location_and_pointer<drag_and_drop_target_drop_item> this_pointer;
+	typedef location_and_pointer<const drag_and_drop_target_drop_item> const_this_pointer;
 
 	drag_and_drop_target_drop_item(const augs::gui::material new_mat);
 
@@ -17,36 +19,7 @@ struct drag_and_drop_target_drop_item : game_gui_rect_node {
 
 	augs::gui::appearance_detector detector;
 
-	template<class C, class gui_element_id>
-	static void draw(C context, const gui_element_id& this_id, augs::gui::draw_info info) {
-		auto mat_coloured = this_id->mat;
-
-		if (this_id->detector.is_hovered)
-			mat_coloured.color.a = 255;
-		else
-			mat_coloured.color.a = 120;
-
-		draw_centered_texture(context, this_id, info, mat_coloured);
-	}
-
-	template<class C, class gui_element_id>
-	static void consume_gui_event(C context, const gui_element_id& this_id, const augs::gui::event_info info) {
-		this_id->detector.update_appearance(info);
-	}
-
-	template<class C, class gui_element_id>
-	static void perform_logic_step(C context, const gui_element_id& this_id, const fixed_delta& dt) {
-		auto& world = context.get_rect_world();
-		auto dragged_item = world.rect_held_by_lmb;
-
-		if (context.alive(dragged_item) && world.held_rect_is_dragged) {
-			this_id->set_flag(augs::gui::flag::ENABLE_DRAWING);
-		}
-		else {
-			this_id->unset_flag(augs::gui::flag::ENABLE_DRAWING);
-		}
-
-		this_id->rc.set_position(context.get_gui_element_component().get_initial_position_for(*this_id) - vec2(20, 20));
-		this_id->rc.set_size((*this_id->mat.tex).get_size() + vec2(40, 40));
-	}
+	static void draw(const const_dispatcher_context& context, const const_this_pointer& this_id, augs::gui::draw_info info);
+	static void consume_gui_event(const dispatcher_context& context, const this_pointer& this_id, const augs::gui::event_info info);
+	static void perform_logic_step(const dispatcher_context&, const this_pointer& this_id, const fixed_delta& dt);
 };
