8c239b164927535a983610074938761d523e829a
geneotech <unknown.unreleased@gmail.com>
2016-12-06 15:50:13 +0100

    fixed include/lib paths

--- game/scene_managers/resource_setups/standard_sound_buffers.cpp
+++ game/scene_managers/resource_setups/standard_sound_buffers.cpp
@@ -1,8 +1,13 @@
 #include "all.h"
 #include "game/resources/manager.h"
 
+#include <sndfile.h>
+
 namespace resource_setups {
 	void load_standard_sound_buffers() {
-
+		{
+			auto& buf = get_resource_manager().create(assets::sound_buffer_id::BILMER2000_MUZZLE);
+			
+		}
 	}
 }
