0b23066f2a1ee07b9acd2d2200be5d8b91c49a53
geneotech <unknown.unreleased@gmail.com>
2016-12-13 23:57:26 +0100

    fixed outrageous bug

--- augs/filesystem/directory.cpp
+++ augs/filesystem/directory.cpp
@@ -5,21 +5,23 @@
 namespace fs = std::experimental::filesystem;
 
 namespace augs {
-	bool create_directory(std::string filename) {
+	bool create_directory(const std::string filename) {
 		return fs::create_directory(filename);
 	}
 	
-	bool create_directories(std::string filename) {
+	bool create_directories(const std::string filename) {
 		return fs::create_directories(filename);
 	}
 
-	std::vector<std::string> get_all_files_in_directory(std::string dirpath) {
+	std::vector<std::string> get_all_files_in_directory(const std::string dirpath) {
 		std::vector<std::string> result;
 
-		for (fs::recursive_directory_iterator i("."), end; i != end; ++i)
-			if (!is_directory(i->path()))
+		for (fs::recursive_directory_iterator i(dirpath), end; i != end; ++i) {
+			if (!is_directory(i->path())) {
 				result.push_back(i->path().filename().generic_string());
+			}
+		}
 
-		return result;
+		return std::move(result);
 	}
 }
