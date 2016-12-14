decee315f8b352b6d94de518c780a574fdc73877
geneotech <unknown.unreleased@gmail.com>
2016-12-14 03:03:29 +0100

    test load

--- game/transcendental/cosmic_movie_director.cpp
+++ game/transcendental/cosmic_movie_director.cpp
@@ -1,18 +1,19 @@
 #include "cosmic_movie_director.h"
 #include "augs/filesystem/directory.h"
+#include "augs/misc/templated_readwrite.h"
 
 #include "augs/templates/string_templates.h"
 
 void cosmic_movie_director::load_recordings_from_directory(const std::string directory_path) {
 	const auto files = augs::get_all_files_in_directory(directory_path);
 
-	for (const auto& f : files) {
-		std::istringstream fname(f);
+	for (const auto& filename_without_directory : files) {
+		std::istringstream f(filename_without_directory);
 		std::string guid_string;
-		std::getline(fname, guid_string, '.');
+		std::getline(f, guid_string, '.');
 
 		const auto guid = to_value<unsigned>(guid_string);
 
-		LOG("%x", guid);
+		guid_to_recording[guid].load_recording(directory_path + filename_without_directory);
 	}
 }
