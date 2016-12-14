129050e920a56b1ddafb2b975d4d4a6a04557ebc
geneotech <unknown.unreleased@gmail.com>
2016-12-14 02:44:13 +0100

    refactored typesafe_sprintf.h

--- augs/misc/typesafe_sprintf.cpp
+++ augs/misc/typesafe_sprintf.cpp
@@ -1,4 +1,1 @@
 #include "typesafe_sprintf.h"
-
-void typesafe_sprintf_detail(size_t, std::string&) {}
-void typesafe_sprintf_detail(size_t, std::wstring&) {}
--- augs/misc/typesafe_sprintf.h
+++ augs/misc/typesafe_sprintf.h
@@ -5,26 +5,31 @@
 #include <type_traits>
 #include <limits>
 
-void typesafe_sprintf_detail(size_t, std::string&);
+template <typename CharType>
+void typesafe_sprintf_detail(size_t, std::basic_string<CharType>&) {
 
-template<typename T, typename... A>
-void typesafe_sprintf_detail(size_t starting_pos, std::string& target_str, T&& val, A&& ...a) {
+}
+
+template<typename CharType, typename T, typename... A>
+void typesafe_sprintf_detail(size_t starting_pos, std::basic_string<CharType>& target_str, T&& val, A&& ...a) {
 	starting_pos = target_str.find('%', starting_pos);
 
 	if (starting_pos != std::string::npos) {
-		std::ostringstream replacement;
+		std::basic_ostringstream<CharType> replacement;
 
-		auto opcode = target_str[starting_pos + 1];
+		const auto opcode = target_str[starting_pos + 1];
 
 		if (opcode == L'f') {
 			replacement << std::fixed;
 			opcode = target_str[starting_pos + 2];
 		}
 
-		if (opcode >= L'0' && opcode <= L'9')
+		if (opcode >= L'0' && opcode <= L'9') {
 			replacement.precision(opcode - L'0');
-		else if (opcode == L'*')
+		}
+		else if (opcode == L'*') {
 			replacement.precision(std::numeric_limits<typename std::decay<T>::type>::digits10);
+		}
 
 		replacement << val;
 		target_str.replace(starting_pos, 2, replacement.str());
@@ -33,42 +38,22 @@ void typesafe_sprintf_detail(size_t starting_pos, std::string& target_str, T&& v
 	typesafe_sprintf_detail(starting_pos, target_str, std::forward<A>(a)...);
 }
 
-template<typename... A>
-std::string typesafe_sprintf(std::string f, A&&... a) {
+template<typename CharType, typename... A>
+std::basic_string<CharType> typesafe_sprintf(std::basic_string<CharType> f, A&&... a) {
 	typesafe_sprintf_detail(0, f, std::forward<A>(a)...);
 	return f;
 }
 
-void typesafe_sprintf_detail(size_t, std::wstring&);
-
-template<typename T, typename... A>
-void typesafe_sprintf_detail(size_t starting_pos, std::wstring& target_str, T&& val, A&& ...a) {
-	starting_pos = target_str.find(L'%', starting_pos);
-
-	if (starting_pos != std::wstring::npos) {
-		std::wostringstream replacement;
-
-		auto opcode = target_str[starting_pos + 1];
-
-		if (opcode == L'f') {
-			replacement << std::fixed;
-			opcode = target_str[starting_pos + 2];
-		}
-
-		if (opcode >= L'0' && opcode <= L'9')
-			replacement.precision(opcode - L'0');
-		else if (opcode == L'*')
-			replacement.precision(std::numeric_limits<typename std::decay<T>::type>::digits10);
-
-		replacement << val;
-		target_str.replace(starting_pos, 2, replacement.str());
-	}
-
-	typesafe_sprintf_detail(starting_pos, target_str, std::forward<A>(a)...);
+template<typename... A>
+std::string typesafe_sprintf(const char* const c_str, A&&... a) {
+	auto f = std::string(c_str);
+	typesafe_sprintf_detail(0, f, std::forward<A>(a)...);
+	return f;
 }
 
 template<typename... A>
-std::wstring typesafe_sprintf(std::wstring f, A&&... a) {
+std::wstring typesafe_sprintf(const wchar_t* const c_str, A&&... a) {
+	auto f = std::wstring(c_str);
 	typesafe_sprintf_detail(0, f, std::forward<A>(a)...);
 	return f;
 }
