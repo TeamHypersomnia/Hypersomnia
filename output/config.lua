0727f4c95dadc6a100ab996197649dcc88d2f9be
geneotech <unknown.unreleased@gmail.com>
2016-12-18 18:17:05 +0100

    fixed sound glitch

--- output/config.lua
+++ output/config.lua
@@ -20,7 +20,7 @@ recording_modes = {
 config_table = {
 	launch_mode = launch_modes.LOCAL,
 	
-	input_recording_mode = recording_modes.DISABLE,
+	input_recording_mode = recording_modes.LIVE,
 	recording_replay_speed = 1,
 
 	determinism_test_cloned_cosmoi_count = 2,
