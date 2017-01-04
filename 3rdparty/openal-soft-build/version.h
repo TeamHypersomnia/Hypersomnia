2a7f947dc66851428480737c69c047a5265ea5e9
geneotech <unknown.unreleased@gmail.com>
2017-01-04 21:35:31 +0100

    build version fix

---
+++ 3rdparty/openal-soft-build/version.h
@@ -0,0 +1,8 @@
+/* Define to the library version */
+#define ALSOFT_VERSION "1.17.2"
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH "HEAD"
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH "40f359d"
