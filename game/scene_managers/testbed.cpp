72134cf76b124b942a8919d55fcd5361c0aacea0
geneotech <unknown.unreleased@gmail.com>
2017-01-04 01:24:36 +0100

    better road

--- game/scene_managers/testbed.cpp
+++ game/scene_managers/testbed.cpp
@@ -72,7 +72,7 @@ namespace scene_managers {
 		const auto car2 = prefabs::create_car(world, components::transform(1490, 340 + 400, -180));
 		const auto car3 = prefabs::create_car(world, components::transform(1490, 340 + 800, -180));
 
-		const auto motorcycle = prefabs::create_motorcycle(world, components::transform(0, 300, -90 + 180));
+		const auto motorcycle = prefabs::create_motorcycle(world, components::transform(250, 400, -90 + 180));
 		//prefabs::create_motorcycle(world, components::transform(100, -600, -90));
 		const auto main_character_motorcycle = prefabs::create_motorcycle(world, components::transform(900, 48200, -90));
 		
