8b9923323503e54d6e50279b1c6ecb0ceb1fb191
geneotech <unknown.unreleased@gmail.com>
2017-03-04 14:29:36 +0100

    removed logs and fixed code formatting

--- application/content_generation/neon_maps.cpp
+++ application/content_generation/neon_maps.cpp
@@ -129,95 +129,122 @@ void make_neon(
 	resize_image(source, vec2u(stamp.radius_towards_x_axis, stamp.radius_towards_y_axis));
 
 	const auto pixel_list = hide_undesired_pixels(source, stamp.light_colors);
-
 	const auto kernel = generate_gauss_kernel(stamp);
 
 	std::vector<rgba> pixel_colors;
-	for (const auto& pixel : pixel_list)
-		pixel_colors.push_back(source.pixel({pixel.x, pixel.y}));
+	
+	for (const auto& pixel : pixel_list) {
+		pixel_colors.push_back(source.pixel({ pixel.x, pixel.y }));
+	}
 
 	size_t i = 0;
 
 	for (auto& pixel : pixel_list) {
 		rgba center_pixel = pixel_colors[i];
 		++i;
-		auto center_pixel_rgba = rgba{ center_pixel[2], center_pixel[1], center_pixel[0], center_pixel[3] };
-		for (size_t y = 0; y < kernel.size(); ++y)
-			for (size_t x = 0; x < kernel[y].size(); ++x)
-			{
+		
+		const auto center_pixel_rgba = rgba{ center_pixel[2], center_pixel[1], center_pixel[0], center_pixel[3] };
+		
+		for (size_t y = 0; y < kernel.size(); ++y) {
+			for (size_t x = 0; x < kernel[y].size(); ++x) {
 				size_t current_index_y = pixel.y + y - stamp.radius_towards_y_axis / 2;
-				if (current_index_y < 0 || current_index_y >= source.get_rows())
+				
+				if (current_index_y < 0 || current_index_y >= source.get_rows()) {
 					continue;
+				}
 
 				size_t current_index_x = pixel.x + x - stamp.radius_towards_x_axis / 2;
-				if (current_index_x < 0 || current_index_x >= source.get_columns())
+				
+				if (current_index_x < 0 || current_index_x >= source.get_columns()) {
 					continue;
+				}
 
 				rgba& current_pixel = source.pixel({ current_index_x, current_index_y });
 				auto current_pixel_rgba = rgba{ current_pixel[2], current_pixel[1], current_pixel[0], current_pixel[3] };
 				size_t alpha = static_cast<size_t>(255 * kernel[y][x] * stamp.amplification);
 				alpha = alpha > 255 ? 255 : alpha;
-				if (current_pixel_rgba == rgba{ 0, 0, 0, 0 } && alpha)
-				{
+
+				if (current_pixel_rgba == rgba{ 0, 0, 0, 0 } && alpha) {
 					current_pixel[2] = center_pixel[2];
 					current_pixel[1] = center_pixel[1];
 					current_pixel[0] = center_pixel[0];
 				}
-				else if (current_pixel_rgba != center_pixel_rgba && alpha)
-				{
-					current_pixel[2] = (alpha * center_pixel[2] + current_pixel[3] * current_pixel[2]) / (alpha + current_pixel[3]);
-					current_pixel[1] = (alpha * center_pixel[1] + current_pixel[3] * current_pixel[1]) / (alpha + current_pixel[3]);
-					current_pixel[0] = (alpha * center_pixel[0] + current_pixel[3] * current_pixel[0]) / (alpha + current_pixel[3]);
-				}
-				if (alpha > current_pixel[3])
-					current_pixel[3] = alpha;
-			}
-	}
 
+				else if (current_pixel_rgba != center_pixel_rgba && alpha) {
+					current_pixel[2] = static_cast<rgba_channel>((alpha * center_pixel[2] + current_pixel[3] * current_pixel[2]) / (alpha + current_pixel[3]));
+					current_pixel[1] = static_cast<rgba_channel>((alpha * center_pixel[1] + current_pixel[3] * current_pixel[1]) / (alpha + current_pixel[3]));
+					current_pixel[0] = static_cast<rgba_channel>((alpha * center_pixel[0] + current_pixel[3] * current_pixel[0]) / (alpha + current_pixel[3]));
+				}
+
+				if (alpha > current_pixel[3]) {
+					current_pixel[3] = static_cast<rgba_channel>(alpha);
+				}
+			}
+		}
+	}
 }
 
 std::vector<std::vector<double>> generate_gauss_kernel(const neon_map_stamp& stamp)
 {
 	const auto radius_towards_x_axis = stamp.radius_towards_x_axis;
 	const auto radius_towards_y_axis = stamp.radius_towards_y_axis;
-	if (!stamp.radius_towards_y_axis)
+
+	if (!stamp.radius_towards_y_axis) {
 		const auto radius_towards_y_axis = radius_towards_x_axis;
+	}
 
 	std::vector<std::vector<std::pair<int, int> > > index;
+
 	auto max_index_x = radius_towards_x_axis / 2;
 	auto max_index_y = radius_towards_y_axis / 2;
 
 	index.resize(radius_towards_y_axis);
-	for (auto& vector : index)
-		vector.resize(radius_towards_x_axis);
 
-	for (size_t y = 0; y < index.size(); ++y)
-		for (size_t x = 0; x < index[y].size(); ++x)
+	for (auto& vector : index) {
+		vector.resize(radius_towards_x_axis);
+	}
+
+	for (size_t y = 0; y < index.size(); ++y) {
+		for (size_t x = 0; x < index[y].size(); ++x) {
 			index[y][x] = std::make_pair(x - max_index_x, y - max_index_y);
+		}
+	}
 
 	std::vector<std::vector<double>> result;
 	result.resize(radius_towards_y_axis);
-	for (auto& vector : result)
-		vector.resize(radius_towards_x_axis);
 
-	for (size_t y = 0; y < result.size(); ++y)
-		for (size_t x = 0; x < result[y].size(); ++x)
+	for (auto& vector : result) {
+		vector.resize(radius_towards_x_axis);
+	}
+
+	for (size_t y = 0; y < result.size(); ++y) {
+		for (size_t x = 0; x < result[y].size(); ++x) {
 			result[y][x] = exp(-1 * (pow(index[x][y].first, 2) + pow(index[x][y].second, 2)) / 2 / pow(stamp.standard_deviation, 2)) / PI_f / 2 / pow(stamp.standard_deviation, 2);
+		}
+	}
 
 	double sum = 0.f;
-	for (const auto& vec_2d : result)
-		for (const auto& value : vec_2d)
-			sum += value;
 
-	for (auto& vector : result)
-		for (auto& value : vector)
+	for (const auto& vec_2d : result) {
+		for (const auto& value : vec_2d) {
+			sum += value;
+		}
+	}
+
+	for (auto& vector : result) {
+		for (auto& value : vector) {
 			value /= sum;
+		}
+	}
 
 	return result;
 }
 
 
-void resize_image(augs::image& image_to_resize, vec2u size) {
+void resize_image(
+	augs::image& image_to_resize, 
+	vec2u size
+) {
 
 	size.x = image_to_resize.get_columns() + size.x * 2;
 	size.y = image_to_resize.get_rows() + size.y * 2;
@@ -225,32 +252,35 @@ void resize_image(augs::image& image_to_resize, vec2u size) {
 	augs::image copy_mat;
 	copy_mat.create(size);
 
-	LOG("image size: %dx%d", copy_mat.get_columns(), copy_mat.get_rows());
-
-
-	int offset_x = static_cast<int>(size.x - image_to_resize.get_columns()) / 2;
-	if (offset_x < 0)
+	auto offset_x = static_cast<int>(size.x - image_to_resize.get_columns()) / 2;
+	
+	if (offset_x < 0) {
 		offset_x = 0;
-	int offset_y = static_cast<int>(size.y - image_to_resize.get_rows()) / 2;
-	if (offset_y < 0)
+	}
+
+	auto offset_y = static_cast<int>(size.y - image_to_resize.get_rows()) / 2;
+	
+	if (offset_y < 0) {
 		offset_y = 0;
+	}
 
-
-	LOG("%s", image_to_resize.get_columns());
-
-	for (size_t y = 0; y < image_to_resize.get_rows(); ++y)
-		for (size_t x = 0; x < image_to_resize.get_columns(); ++x)	
+	for (size_t y = 0; y < image_to_resize.get_rows(); ++y) {
+		for (size_t x = 0; x < image_to_resize.get_columns(); ++x)
 		{
 			copy_mat.pixel({ x + offset_x, y + offset_y }) = image_to_resize.pixel({ x, y });;
 		}
+	}
 
 	image_to_resize = copy_mat;
 }
 
-std::vector<vec2u> hide_undesired_pixels(augs::image& original_image, const std::vector<rgba>& color_whitelist) {
+std::vector<vec2u> hide_undesired_pixels(
+	augs::image& original_image, 
+	const std::vector<rgba>& color_whitelist
+) {
 	std::vector<vec2u> result;
 
-	for (size_t y = 0; y < original_image.get_rows(); ++y)
+	for (size_t y = 0; y < original_image.get_rows(); ++y) {
 		for (size_t x = 0; x < original_image.get_columns(); ++x)
 		{
 			auto& pixel = original_image.pixel({ x, y });
@@ -262,9 +292,11 @@ std::vector<vec2u> hide_undesired_pixels(augs::image& original_image, const std:
 			{
 				pixel = { 0,0,0,0 };
 			}
-			else
+			else {
 				result.push_back(vec2u{ x, y });
+			}
 		}
+	}
 
 	return result;
 }
