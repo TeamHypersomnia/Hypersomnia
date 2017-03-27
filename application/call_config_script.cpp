1358c9e48af2661421df0a1d9d41ebd76fcb0897
geneotech <unknown.unreleased@gmail.com>
2017-03-27 21:10:11 +0200

    went back to config.local.lua solution

--- application/call_config_script.cpp
+++ application/call_config_script.cpp
@@ -16,11 +16,13 @@ void call_window_script(
 void call_config_script(
 	augs::lua_state_raii& lua,
 	const std::string& config_lua_path,
-	const std::string& config_override_lua_path
+	const std::string& config_local_lua_path
 ) {
-	lua.dofile_and_report_errors(config_lua_path);
+	std::string used_filename = config_lua_path;
 
-	if (augs::file_exists(config_override_lua_path)) {
-		lua.dofile_and_report_errors(config_override_lua_path);
+	if (augs::file_exists(config_local_lua_path)) {
+		used_filename = config_local_lua_path;
 	}
+
+	lua.dofile_and_report_errors(used_filename);
 }
--- application/call_config_script.h
+++ application/call_config_script.h
@@ -8,7 +8,7 @@ namespace augs {
 void call_config_script(
 	augs::lua_state_raii& lua, 
 	const std::string& config_lua_path, 
-	const std::string& config_override_lua_path
+	const std::string& config_local_lua_path
 );
 
 void call_window_script(
