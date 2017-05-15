1a3fe60b362d30f0d393d33f1edcf7854adc2138
geneotech <unknown.unreleased@gmail.com>
2017-05-15 15:26:51 +0200

    fix

--- src/augs/templates/conditional_to_string.h
+++ src/augs/templates/conditional_to_string.h
@@ -5,7 +5,7 @@
 template <class T>
 std::string conditional_to_string(
 	const T&, 
-	std::enable_if_t<!can_stream_left<std::ostringstream, T>::value>* dummy = nullptr
+	std::enable_if_t<!can_stream_left_v<std::ostringstream, T>>* dummy = nullptr
 ) {
 	return {};
 }
@@ -13,7 +13,7 @@ std::string conditional_to_string(
 template <class T>
 std::string conditional_to_string(
 	const T& t, 
-	std::enable_if_t<can_stream_left<std::ostringstream, T>::value>* dummy = nullptr
+	std::enable_if_t<can_stream_left_v<std::ostringstream, T>>* dummy = nullptr
 ) {
 	std::ostringstream out;
 	out << t;
