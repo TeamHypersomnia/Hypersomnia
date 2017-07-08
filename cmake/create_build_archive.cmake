ec39e2331ee9c620d4c1b2de8d126fe192f12aa2
geneotech <unknown.unreleased@gmail.com>
2017-07-08 12:47:26 +0200

    cmake comments & renames

--- cmake/create_build_archive.cmake
+++ cmake/create_build_archive.cmake
@@ -16,7 +16,7 @@ if(WIN32)
 		# and the WinRar command breaks for some reason.
 
 		# First we configure the script to use the correct input and output paths.
-		# This should substitute ARCHIVER_EXECUTABLE_PATH, NEW_ARCHIVE_OUTPUT_PATH, RESOURCES_DIR, RESOURCE_FOLDER_NAME and EXE_PATH.
+		# This should substitute ARCHIVER_EXECUTABLE_PATH, NEW_ARCHIVE_OUTPUT_PATH, RESOURCES_DIR, RESOURCES_FOLDER_NAME and EXE_PATH.
 
 		configure_file(
 			${CREATE_ARCHIVE_PY_PATH}
