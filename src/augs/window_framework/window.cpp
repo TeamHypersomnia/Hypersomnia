aa0b0e1aa71d707fe89e180eb2d0162f55555939
geneotech <unknown.unreleased@gmail.com>
2017-10-07 17:19:20 +0200

    Added PLATFORM_LINUX stubs

--- src/augs/window_framework/window.cpp
+++ src/augs/window_framework/window.cpp
@@ -486,6 +486,50 @@ namespace augs {
 	}
 }
 
+#elif PLATFORM_LINUX
+namespace augs {
+	window::window(const window_settings& settings) {
+		apply(settings, true);
+	}
+
+	void window::set_window_name(const std::string& name) {}
+	void window::set_window_border_enabled(const bool) {}
+
+	bool window::swap_buffers() { return true; }
+
+	void window::show() {}
+	void window::set_mouse_position_frozen(const bool) {}
+
+	void window::collect_entropy(local_entropy& into) {}
+
+	void window::set_window_rect(const xywhi) {}
+	xywhi window::get_window_rect() const { return {}; }
+
+	bool window::is_active() const { return false; }
+	void window::destroy() {}
+
+	bool window::set_as_current_impl() {
+#if BUILD_OPENGL
+		return true;
+#else
+		return true;
+#endif
+	}
+
+	void window::set_current_to_none_impl() {
+#if BUILD_OPENGL
+
+#endif
+	}
+
+	std::optional<std::string> window::get_open_file_name(const wchar_t* const filter) const {
+		return std::nullopt;
+	}
+
+	std::optional<std::string> window::get_save_file_name(const wchar_t* const filter) const {
+		return std::nullopt;
+	}
+}
 #else
 namespace augs {
 	window::window(const window_settings& settings) {
