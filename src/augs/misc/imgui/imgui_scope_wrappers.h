96d529c41d04ac599321da8243d01ce35433a6f0
geneotech <unknown.unreleased@gmail.com>
2017-10-29 19:28:11 +0100

    #249 fixed linker errors

--- src/augs/misc/imgui/imgui_scope_wrappers.h
+++ src/augs/misc/imgui/imgui_scope_wrappers.h
@@ -6,37 +6,37 @@
 
 namespace augs {
 	namespace imgui {
-		auto scoped_indent() {
+		inline auto scoped_indent() {
 			ImGui::Indent();
 		
 			return make_scope_guard([]() { ImGui::Unindent(); });
 		}
 		
-		auto scoped_tooltip() {
+		inline auto scoped_tooltip() {
 			ImGui::BeginTooltip();
 		
 			return make_scope_guard([]() { ImGui::EndTooltip(); });
 		}
 		
-		auto scoped_item_width(const float v) {
+		inline auto scoped_item_width(const float v) {
 			ImGui::PushItemWidth(v);
 		
 			return make_scope_guard([]() { ImGui::PopItemWidth(); });
 		}
 
-		auto scoped_id(const int v) {
+		inline auto scoped_id(const int v) {
 			ImGui::PushID(v);
 
 			return make_scope_guard([]() { ImGui::PopID(); });
 		}
 
-		auto scoped_id(void* const v) {
+		inline auto scoped_id(void* const v) {
 			ImGui::PushID(v);
 
 			return make_scope_guard([]() { ImGui::PopID(); });
 		}
 		
-		auto scoped_tree_node(const char* label) {
+		inline auto scoped_tree_node(const char* label) {
 			const auto result = ImGui::TreeNode(label);
 		
 			auto opt = std::make_optional(make_scope_guard([result]() { if (result) { ImGui::TreePop(); }}));
@@ -48,7 +48,7 @@ namespace augs {
 			return opt;
 		}
 
-		auto scoped_menu(const char* label, const bool enabled = true) {
+		inline auto scoped_menu(const char* label, const bool enabled = true) {
 			const auto result = ImGui::BeginMenu(label, enabled);
 
 			auto opt = std::make_optional(make_scope_guard([result]() { if (result) { ImGui::EndMenu(); }}));
@@ -60,7 +60,7 @@ namespace augs {
 			return opt;
 		}
 
-		auto scoped_main_menu_bar() {
+		inline auto scoped_main_menu_bar() {
 			const auto result = ImGui::BeginMainMenuBar();
 
 			auto opt = std::make_optional(make_scope_guard([result]() { if (result) { ImGui::EndMainMenuBar(); }}));
@@ -72,7 +72,7 @@ namespace augs {
 			return opt;
 		}
 
-		auto scoped_tab_menu_bar(const float y) {
+		inline auto scoped_tab_menu_bar(const float y) {
 			const auto result = ImGui::BeginTabMenuBar(y);
 
 			auto opt = std::make_optional(make_scope_guard([result]() { if (result) { ImGui::EndTabMenuBar(); }}));
