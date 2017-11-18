c868c0546103b8896abf6b9a95104472e6596fa3
Patryk Czachurski <patryk.czachurski@gmail.com>
2017-11-18 14:36:11 +0100

    Better builder scripts, removed heresy

--- sh/unix/make.sh
+++
@@ -1,7 +0,0 @@
-source sh/unix/common.sh
-
-pushd $(build_dir)
-make -j4 
-MAKE_EXIT_CODE=$?
-popd
-exit $MAKE_EXIT_CODE
--- sh/unix/run_cgdb.sh
+++
@@ -1,7 +0,0 @@
-source sh/unix/launcher.sh 
-
-function standard_launcher() {
-	cgdb $1	
-}
-
-launcher standard_launcher
