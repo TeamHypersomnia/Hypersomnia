bced972a8118d8064dd2dc27b09e0237b85362ec
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-01-11 14:16:27 +0100

    rm machine_entropy.cpp

--- src/augs/misc/machine_entropy.h
+++ src/augs/misc/machine_entropy.h
@@ -7,26 +7,4 @@
 namespace augs {
 	using local_entropy = std::vector<event::change>;
 	using remote_entropy = std::vector<event::change>;
-
-#if 0
-	struct machine_entropy {
-		using local_type = std::vector<augs::event::change>;
-		using remote_type = std::vector<augs::network::message>;
-
-		// GEN INTROSPECTOR struct augs::machine_entropy
-		local_type local;
-		remote_type remote;
-		// END GEN INTROSPECTOR
-
-		// here will be remote entropy as well
-
-		machine_entropy& operator+=(const machine_entropy&);
-		
-		void clear();
-
-		void apply_to(event::state&) const;
-		bool operator==(const machine_entropy&) const;
-		bool empty() const;
-	};
-#endif
 }
