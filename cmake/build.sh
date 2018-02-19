060e8a288b86614f5dc62a8484e728d3026315a2
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-02-19 01:26:17 +0100

    successfully built both clang and gcc versions

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash 
 CONFIGURATION=$1
 ARCHITECTURE=$2
+C_COMPILER=$3
+CXX_COMPILER=$4
+
+if [[ ! -z "$3" ]] && [[ -z "$4" ]]
+then
+	echo "You must specify both a C and a C++ compiler, or leave both unspecified."
+fi
 
 if [[ -z "$ARCHITECTURE" ]]
 then
 	ARCHITECTURE="x64"
 fi
 
-TARGET_DIR="build/${CONFIGURATION}-${ARCHITECTURE}"
+if [[ -z "$C_COMPILER" ]]
+then
+	C_COMPILER="clang"
+	CXX_COMPILER="clang++"
+fi
+
+TARGET_DIR="build/${CONFIGURATION}-${ARCHITECTURE}-${C_COMPILER}"
 echo "Building into $TARGET_DIR"
 
 mkdir --parents $TARGET_DIR
 cd $TARGET_DIR
-cmake -DARCHITECTURE=$ARCHITECTURE -DCMAKE_BUILD_TYPE=$CONFIGURATION $3 $OLDPWD
+
+export CC=$C_COMPILER
+export CXX=$CXX_COMPILER
+
+cmake -DARCHITECTURE=$ARCHITECTURE -DCMAKE_BUILD_TYPE=$CONFIGURATION $@ $OLDPWD
