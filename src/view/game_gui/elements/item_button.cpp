963f27af774201d0858cdd2e413940d455309a20
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-03-18 14:49:15 +0100

    some strange behaviour in item button

--- src/view/game_gui/elements/item_button.cpp
+++ src/view/game_gui/elements/item_button.cpp
@@ -276,6 +276,11 @@ void item_button::draw_proc(
 				const auto iteration_lambda = [&](const const_entity_handle desc) {
 					const auto parent_slot = cosmos[desc.get<components::item>().get_current_slot()];
 
+					if (!attachment_index < layout.boxes.size()) {
+						// LOG("Excuse me, but something's gone haywire.");
+						return recursive_callback_result::ABORT;
+					}
+
 					if (parent_slot.is_physically_connected_until(item)) {
 						auto attachment_sprite = desc.get<invariants::sprite>();
 
