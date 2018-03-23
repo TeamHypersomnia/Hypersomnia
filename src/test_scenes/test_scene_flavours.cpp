b5670ffbec6b6a24cc47b6d56c2606aa3b0bb8e1
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-03-23 19:02:35 +0100

    cuts in testbed

--- src/test_scenes/test_scene_flavours.cpp
+++ src/test_scenes/test_scene_flavours.cpp
@@ -6,12 +6,12 @@
 #include "augs/templates/format_enum.h"
 
 void populate_test_scene_flavours(const loaded_game_image_caches& logicals, all_entity_flavours& into) {
-	test_flavours::populate_grenade_types(logicals, into);
-	test_flavours::populate_character_types(logicals, into);
-	test_flavours::populate_gun_types(logicals, into);
-	test_flavours::populate_other_types(logicals, into);
-	test_flavours::populate_car_types(logicals, into);
-	test_flavours::populate_crate_types(logicals, into);
-	test_flavours::populate_melee_types(logicals, into);
-	test_flavours::populate_backpack_types(logicals, into);
+	test_flavours::populate_grenade_flavours(logicals, into);
+	test_flavours::populate_character_flavours(logicals, into);
+	test_flavours::populate_gun_flavours(logicals, into);
+	test_flavours::populate_other_flavours(logicals, into);
+	test_flavours::populate_car_flavours(logicals, into);
+	test_flavours::populate_crate_flavours(logicals, into);
+	test_flavours::populate_melee_flavours(logicals, into);
+	test_flavours::populate_backpack_flavours(logicals, into);
 }
