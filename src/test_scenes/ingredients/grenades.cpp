fe59dd0bcd81a212bee7df3f5145a40e1e28abf5
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-04-11 15:07:02 +0200

    fix test impulses

--- src/test_scenes/ingredients/grenades.cpp
+++ src/test_scenes/ingredients/grenades.cpp
@@ -102,7 +102,7 @@ namespace test_flavours {
 			in.inner_ring_color = yellow;
 			in.outer_ring_color = orange;
 			in.effective_radius = 450.f;
-			in.impact_impulse = 550.f;
+			in.impact_impulse = 2.f;
 			in.sound_gain = 2.2f;
 			in.sound_effect = assets::sound_buffer_id::INTERFERENCE_EXPLOSION;
 			in.type = adverse_element_type::INTERFERENCE;
@@ -149,7 +149,7 @@ namespace test_flavours {
 			in.inner_ring_color = cyan;
 			in.outer_ring_color = turquoise;
 			in.effective_radius = 350.f;
-			in.impact_impulse = 20.f;
+			in.impact_impulse = 2.f;
 			in.sound_gain = 2.2f;
 			in.sound_effect = assets::sound_buffer_id::PED_EXPLOSION;
 			in.type = adverse_element_type::PED;
