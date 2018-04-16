71d5d70760bc0c4bf692803f48bbecba68c2ab3b
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-04-16 16:23:08 +0200

    some attempts at refactor

--- src/application/setups/editor/property_editor/compare_all_fields_to.h
+++ src/application/setups/editor/property_editor/compare_all_fields_to.h
@@ -8,6 +8,35 @@ namespace augs {
 	struct trivial_type_marker;
 };
 
+template <class M>
+struct comparator {
+	bool& equal;
+	const M& first;
+
+	template <class... Args>
+	auto operator()(Args&&...) const {
+		return callback_result::CONTINUE;
+	}
+
+	auto operator()(const M& resolved) const {
+		if(!(first == resolved)) {
+			equal = false;
+			return callback_result::ABORT;
+		}
+
+		return callback_result::CONTINUE;
+	}
+
+	auto operator()(const augs::trivial_type_marker& resolved) const {
+		if(!(first == *reinterpret_cast<const M*>(std::addressof(resolved)))) {
+			equal = false;
+			return callback_result::ABORT;
+		}
+
+		return callback_result::CONTINUE;
+	}
+};
+
 template <class M, class id_type, class Container>
 bool compare_all_fields_to(
 	const M& first,
@@ -22,28 +51,7 @@ bool compare_all_fields_to(
 		cosm, 
 		type_id, 
 		ids, 
-		[&equal, &first](const auto& resolved) {
-			using T = std::decay_t<decltype(resolved)>;
-
-			bool this_equals = true;
-
-			if constexpr(std::is_same_v<T, augs::trivial_type_marker>) {
-				this_equals = first == *reinterpret_cast<const M*>(std::addressof(resolved));
-			}
-			else if constexpr(std::is_same_v<T, M>) {
-				this_equals = first == resolved;
-			}
-			else {
-				/* Do not compile the lambda for other types */
-			}
-
-			if (!this_equals) {
-				equal = false;
-				return callback_result::ABORT;
-			}
-
-			return callback_result::CONTINUE;
-		}
+		comparator<M> { equal, first }
 	);
 
 	return equal;
