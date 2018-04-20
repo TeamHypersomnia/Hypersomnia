6d70157e5085c28fa6454fdf017b250d7cc8bd0b
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-04-20 15:13:29 +0200

    also for lua

--- src/augs/readwrite/lua_readwrite_overload_traits.h
+++ src/augs/readwrite/lua_readwrite_overload_traits.h
@@ -5,48 +5,48 @@
 #define LUA_READWRITE_OVERLOAD_TRAITS_INCLUDED 1
 
 namespace augs {
-	template <class Serialized, class = void>
+	template <class ArgsList, class = void>
 	struct has_lua_read_overload : std::false_type 
 	{};
 
-	template <class Serialized>
+	template <class... Args>
 	struct has_lua_read_overload <
-		Serialized,
+		type_list<Args...>,
 		decltype(
 			read_object_lua(
 				std::declval<sol::table>(),
-				std::declval<Serialized&>()
+				std::declval<Args&>()...
 			),
 			void()
 		)
 	> : std::true_type 
 	{};
 
-	template <class Serialized, class = void>
+	template <class ArgsList, class = void>
 	struct has_lua_write_overload : std::false_type 
 	{};
 
-	template <class Serialized>
+	template <class... Args>
 	struct has_lua_write_overload <
-		Serialized,
+		type_list<Args...>,
 		decltype(
 			write_object_lua(
 				std::declval<sol::table>(),
-				std::declval<const Serialized&>()
+				std::declval<Args&>()...
 			),
 			void()
 		)
 	> : std::true_type 
 	{};
 
-	template <class Serialized>
-	constexpr bool has_lua_read_overload_v = has_lua_read_overload<Serialized>::value;
+	template <class... Args>
+	constexpr bool has_lua_read_overload_v = has_lua_read_overload<type_list<Args...>>::value;
 
-	template <class Serialized>
-	constexpr bool has_lua_write_overload_v = has_lua_write_overload<Serialized>::value;
+	template <class... Args>
+	constexpr bool has_lua_write_overload_v = has_lua_write_overload<type_list<Args...>>::value;
 
-	template <class Serialized>
+	template <class... Args>
 	constexpr bool has_lua_readwrite_overloads_v = 
-		has_lua_read_overload_v<Serialized> && has_lua_write_overload_v<Serialized>
+		has_lua_read_overload_v<Args...> && has_lua_write_overload_v<Args...>
 	;
 }
