eaea2d0ef63d52090f72f39e6caff48a3ae0692e
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-05-30 21:16:31 +0200

    Fixed neon maps for vindicator, added offsets to bilmer

--- hypersomnia/content/official/gfx/bilmer2000.meta.lua
+++ hypersomnia/content/official/gfx/bilmer2000.meta.lua
@@ -16,14 +16,82 @@ return {
         y = 80
       },
       standard_deviation = 6
-    }
+    },
+    generate_desaturation = false
   },
   offsets = {
     gun = {
       bullet_spawn = {
         x = 0,
         y = -2
+      },
+      detachable_magazine = {
+        pos = {
+          x = -13,
+          y = 6
+        },
+        rotation = 90
       }
-  	}
+    },
+    item = {
+      attachment_anchor = {
+        x = 0,
+        y = 0
+      },
+      back_anchor = {
+        x = 0,
+        y = 0
+      },
+      hand_anchor = {
+        x = -26,
+        y = -1
+      }
+    },
+    legs = {
+      foot = {
+        x = 0,
+        y = 0
+      }
+    },
+    torso = {
+      back = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      head = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      primary_hand = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      secondary_hand = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    }
+  },
+  usage_as_button = {
+    bbox_expander = {
+      x = 0,
+      y = 0
+    },
+    flip = {
+      horizontally = false,
+      vertically = false
+    }
   }
 }
