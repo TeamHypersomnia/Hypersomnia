de03312d34fceb3bcdba53d502f923855bc73d75
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-06-21 21:50:05 +0200

    fix gcc warning about strict aliasing

--- src/3rdparty/Box2D/Common/b2Math.h
+++ src/3rdparty/Box2D/Common/b2Math.h
@@ -25,8 +25,8 @@
 /// This function is used to ensure that a floating point number is not a NaN or infinity.
 inline bool b2IsValid(float32 x)
 {
-	int32 ix = *reinterpret_cast<int32*>(&x);
-	return (ix & 0x7f800000) != 0x7f800000;
+	const int32* ix = reinterpret_cast<const int32*>(&x);
+	return (*ix & 0x7f800000) != 0x7f800000;
 }
 
 /// This is a approximate yet fast inverse square-root.
