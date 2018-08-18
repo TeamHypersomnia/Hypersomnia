676cc855abf1f31cadaddc775dd3744fc3371642
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-08-19 00:48:04 +0200

    New heavy shooting sprites

---
+++ hypersomnia/content/official/gfx/resistance_torso_heavy_shoot_4.meta.lua
@@ -0,0 +1,122 @@
+return {
+  extra_loadables = {
+    enabled_generate_neon_map = {
+      alpha_multiplier = 0.40000000596046448,
+      amplification = 100,
+      light_colors = {
+        "223 113 38 255"
+      },
+      radius = {
+        x = 80,
+        y = 80
+      },
+      standard_deviation = 6
+    },
+    generate_desaturation = false
+  },
+  offsets = {
+    gun = {
+      bullet_spawn = {
+        x = 0,
+        y = 0
+      },
+      detachable_magazine = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    },
+    item = {
+      attachment_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      back_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      beep_offset = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      hand_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      head_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    },
+    legs = {
+      foot = {
+        x = 0,
+        y = 0
+      }
+    },
+    torso = {
+      back = {
+        pos = {
+          x = -15,
+          y = -8
+        },
+        rotation = 29
+      },
+      head = {
+        pos = {
+          x = 1,
+          y = -3
+        },
+        rotation = -13
+      },
+      legs = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      primary_hand = {
+        pos = {
+          x = 3,
+          y = 36
+        },
+        rotation = -50.527542114257812
+      },
+      secondary_hand = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    }
+  },
+  usage_as_button = {
+    bbox_expander = {
+      x = 0,
+      y = 0
+    },
+    flip = {
+      horizontally = false,
+      vertically = false
+    }
+  }
+}
---
+++ hypersomnia/content/official/gfx/resistance_torso_heavy_shoot_5.meta.lua
@@ -0,0 +1,122 @@
+return {
+  extra_loadables = {
+    enabled_generate_neon_map = {
+      alpha_multiplier = 0.40000000596046448,
+      amplification = 100,
+      light_colors = {
+        "223 113 38 255"
+      },
+      radius = {
+        x = 80,
+        y = 80
+      },
+      standard_deviation = 6
+    },
+    generate_desaturation = false
+  },
+  offsets = {
+    gun = {
+      bullet_spawn = {
+        x = 0,
+        y = 0
+      },
+      detachable_magazine = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    },
+    item = {
+      attachment_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      back_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      beep_offset = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      hand_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      head_anchor = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    },
+    legs = {
+      foot = {
+        x = 0,
+        y = 0
+      }
+    },
+    torso = {
+      back = {
+        pos = {
+          x = -15,
+          y = -9
+        },
+        rotation = 30
+      },
+      head = {
+        pos = {
+          x = 2,
+          y = -3
+        },
+        rotation = -18
+      },
+      legs = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      },
+      primary_hand = {
+        pos = {
+          x = 2,
+          y = 43
+        },
+        rotation = -53.130104064941406
+      },
+      secondary_hand = {
+        pos = {
+          x = 0,
+          y = 0
+        },
+        rotation = 0
+      }
+    }
+  },
+  usage_as_button = {
+    bbox_expander = {
+      x = 0,
+      y = 0
+    },
+    flip = {
+      horizontally = false,
+      vertically = false
+    }
+  }
+}
