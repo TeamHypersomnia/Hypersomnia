05b2b16ec7ffa9e611b03060bd154e8c8d20990d
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-12-16 22:26:58 +0100

    Bugfix for arena gui mixin in release mode. Accept the player id by value, not by reference.

--- src/view/mode_gui/draw_mode_gui_input.h
+++ src/view/mode_gui/draw_mode_gui_input.h
@@ -1,14 +1,14 @@
 #pragma once
+#include "game/modes/mode_player_id.h"
 
 class images_in_atlas_map;
-struct mode_player_id;
 struct mode_entropy;
 
 struct config_lua_table;
 
 struct draw_mode_gui_input {
 	const float game_screen_top;
-	const mode_player_id& local_player_id;
+	const mode_player_id local_player_id;
 	const images_in_atlas_map& images_in_atlas;
 	const config_lua_table& config;
 };
