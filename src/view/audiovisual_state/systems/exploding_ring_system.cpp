966917b7514862352f8db2dd346b0b60e2d060a8
Patryk Czachurski <patryk.czachurski@gmail.com>
2019-08-18 23:16:27 +0200

    Fix explosion body zooming

--- src/view/audiovisual_state/systems/exploding_ring_system.cpp
+++ src/view/audiovisual_state/systems/exploding_ring_system.cpp
@@ -231,7 +231,7 @@ void exploding_ring_system::draw_highlights_of_rings(
 		const auto passed = global_time_seconds - r.time_of_occurence_seconds;
 		auto ratio = passed / (r.in.maximum_duration_seconds * 1.2);
 
-		const auto radius = std::max(r.in.outer_radius_end_value, r.in.outer_radius_start_value) / cone.eye.zoom;
+		const auto radius = std::max(r.in.outer_radius_end_value, r.in.outer_radius_start_value);
 		auto highlight_col = r.in.color;
 
 		const auto highlight_amount = 1.f - ratio;
