8353325daf33bbc1c54e573e37373fea17e14b46
geneotech <patryk.czachurski@gmail.com>
2020-04-14 02:40:03 +0200

    Balance cyberspray and zamiec

--- src/test_scenes/ingredients/guns.cpp
+++ src/test_scenes/ingredients/guns.cpp
@@ -3495,7 +3495,7 @@ namespace test_flavours {
 			gun_def.low_ammo_cue_sound.id = to_sound_id(test_scene_sound_id::LOW_AMMO_CUE);
 			gun_def.kickback_towards_wielder = kickback_mult * 2.f;
 			gun_def.adversarial.knockout_award = static_cast<money_type>(600);
-			gun_def.recoil_multiplier = 0.25;
+			gun_def.recoil_multiplier = 0.3f;
 
 			gun_def.maximum_heat = 2.f;
 			gun_def.gunshot_adds_heat = 0.052f;
@@ -3512,7 +3512,7 @@ namespace test_flavours {
 			test_flavours::add_lying_item_dynamic_body(meta);
 			set_density_mult(meta, 0.9f);
 			make_default_gun_container(meta, item_holding_stance::PISTOL_LIKE, 1200.f, 0.f, false);
-			meta.get<invariants::item>().standard_price = 2050;
+			meta.get<invariants::item>().standard_price = 2100;
 			set_chambering_duration_ms(meta, 600.f);
 
 			auto& item = meta.get<invariants::item>();
@@ -3556,7 +3556,7 @@ namespace test_flavours {
 			gun_def.low_ammo_cue_sound.id = to_sound_id(test_scene_sound_id::LOW_AMMO_CUE);
 			gun_def.kickback_towards_wielder = kickback_mult * 1.f;
 			gun_def.adversarial.knockout_award = static_cast<money_type>(450);
-			gun_def.recoil_multiplier = 0.25;
+			gun_def.recoil_multiplier = 0.2;
 
 			gun_def.maximum_heat = 2.f;
 			gun_def.gunshot_adds_heat = 0.042f;
@@ -3572,7 +3572,7 @@ namespace test_flavours {
 			test_flavours::add_lying_item_dynamic_body(meta);
 			set_density_mult(meta, 1.2f);
 			make_default_gun_container(meta, item_holding_stance::PISTOL_LIKE, 1000.f, 0.f, false);
-			meta.get<invariants::item>().standard_price = 2500;
+			meta.get<invariants::item>().standard_price = 2400;
 			set_chambering_duration_ms(meta, 300.f);
 
 			auto& item = meta.get<invariants::item>();
