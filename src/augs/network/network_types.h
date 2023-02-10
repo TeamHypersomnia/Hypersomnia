d7129f9e02cad6eb56379669fcec553a0a446ebc
geneotech <patryk.czachurski@gmail.com>
2023-02-10 01:31:40 +0100

    Allow 1-char nicknames

--- src/augs/network/network_types.h
+++ src/augs/network/network_types.h
@@ -15,7 +15,7 @@ constexpr std::size_t max_mode_players_v = max_incoming_connections_v + 1;
 constexpr std::size_t max_rcon_password_length_v = 30;
 constexpr std::size_t max_chat_message_length_v = 180;
 
-constexpr std::size_t min_nickname_length_v = 3;
+constexpr std::size_t min_nickname_length_v = 1;
 constexpr std::size_t max_nickname_length_v = 30;
 constexpr std::size_t max_server_name_length_v = 60;
 
