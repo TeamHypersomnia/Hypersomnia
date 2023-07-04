4fe65cced55fd518f598c08484394b1f52283457
geneotech <patryk.czachurski@gmail.com>
2023-07-04 16:26:59 +0200

    Switch back to clang-14 on linux

--- cmake/linux_actions/install_llvm.sh
+++ cmake/linux_actions/install_llvm.sh
@@ -1,4 +1,4 @@
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository -y "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-15 main"
+sudo add-apt-repository -y "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main"
 sudo apt-get -q update
-sudo apt-get -y install clang-15 lld-15 libc++-15-dev libc++abi-15-dev
+sudo apt-get -y install clang-14 lld-14 libc++-14-dev libc++abi-14-dev
