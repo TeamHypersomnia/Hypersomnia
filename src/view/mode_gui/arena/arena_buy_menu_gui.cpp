28a5d4e33aa0774d56d8305a1831365ac6fff035
geneotech <patryk.czachurski@gmail.com>
2023-08-30 15:17:20 +0200

    Fix double backpacks in buy menu

--- src/view/mode_gui/arena/arena_buy_menu_gui.cpp
+++ src/view/mode_gui/arena/arena_buy_menu_gui.cpp
@@ -922,11 +922,7 @@ result_type arena_buy_menu_gui::perform_imgui(const input_type in) {
 			auto for_each_tool = [&](auto&& callback) {
 				cosm.for_each_flavour_having<invariants::tool>(
 					[&](const auto& id, const auto& flavour) {
-						if (::is_backpack_like(flavour)) {
-							callback(id, flavour);
-						}
-
-						if (!::is_armor_like(flavour)) {
+						if (::is_backpack_like(flavour) || !::is_armor_like(flavour)) {
 							callback(id, flavour);
 						}
 					}
