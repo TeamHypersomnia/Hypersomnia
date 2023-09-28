35803e001c9f857fe5db33c6874f6874d5ff1229
geneotech <patryk.czachurski@gmail.com>
2023-09-28 19:35:09 +0200

    Crash with caki test fix: use LLVM 16.

--- cmake/linux_actions/install_llvm.sh
+++ cmake/linux_actions/install_llvm.sh
@@ -1,4 +1,4 @@
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository -y "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main"
+sudo add-apt-repository -y "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-16 main"
 sudo apt-get -q update
-sudo apt-get -y install clang-14 lld-14 libc++-14-dev libc++abi-14-dev
+sudo apt-get -y install clang-16 lld-16 libc++-16-dev libc++abi-16-dev
