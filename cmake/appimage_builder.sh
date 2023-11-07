15d6b1fc50d0bec141d6966c773ac8642011e6ef
geneotech <patryk.czachurski@gmail.com>
2023-11-07 02:04:18 +0100

    Force strip the executable for the AppImage

--- cmake/appimage_builder.sh
+++ cmake/appimage_builder.sh
@@ -10,6 +10,9 @@ mkdir /tmp/AppDir/usr/lib -p
 cp build/current/Hypersomnia /tmp/AppDir/usr/bin
 cp build/current/libsteam_integration.so /tmp/AppDir/usr/lib
 
+strip /tmp/AppDir/usr/bin/Hypersomnia
+strip /tmp/AppDir/usr/lib/libsteam_integration.so
+
 pushd /tmp/AppDir/usr/bin
 	# So linuxdeploy can find it
 	ln -s ../lib/libsteam_integration.so
@@ -32,4 +35,4 @@ rm -rf /tmp/AppDir/usr/share/hypersomnia/logs
 rm -rf /tmp/AppDir/usr/share/hypersomnia/cache
 
 # Create the AppImage.
-OUTPUT="Hypersomnia.AppImage" ARCH=x86_64 ./linuxdeploy-x86_64.AppImage --appdir=/tmp/AppDir --icon-file=hypersomnia/content/gfx/metropolis_square_logo.png --icon-filename=Hypersomnia --desktop-file=cmake/Hypersomnia.desktop --executable=build/current/Hypersomnia --custom-apprun=cmake/AppRun --output=appimage
+OUTPUT="Hypersomnia.AppImage" ARCH=x86_64 ./linuxdeploy-x86_64.AppImage --appdir=/tmp/AppDir --icon-file=hypersomnia/content/gfx/metropolis_square_logo.png --icon-filename=Hypersomnia --desktop-file=cmake/Hypersomnia.desktop --executable=/tmp/AppDir/usr/bin/Hypersomnia --custom-apprun=cmake/AppRun --output=appimage
