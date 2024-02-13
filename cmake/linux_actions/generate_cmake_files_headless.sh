9b6d8eb1cb36b319f24277476273369d100299a0
geneotech <patryk.czachurski@gmail.com>
2024-02-13 20:10:26 +0100

    Simplify some CMake flags

--- cmake/linux_actions/generate_cmake_files_headless.sh
+++ cmake/linux_actions/generate_cmake_files_headless.sh
@@ -1,6 +1,6 @@
 c_compiler=clang-16
 cxx_compiler=clang++-16
-other_cmake_flags="-DHEADLESS=1 -DBUILD_DEBUGGER_SETUP=0"
+other_cmake_flags="-DHEADLESS=1"
 
 ${c_compiler} -v && ${cxx_compiler} -v && cmake --version
 export CC=${c_compiler}
