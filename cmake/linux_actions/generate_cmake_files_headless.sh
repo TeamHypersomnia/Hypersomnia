4ddb1dceb3871430a5d799e2e8c907bba7747725
geneotech <patryk.czachurski@gmail.com>
2024-02-13 21:12:54 +0100

    Use the new installed openssl [ci skip]

--- cmake/linux_actions/generate_cmake_files_headless.sh
+++ cmake/linux_actions/generate_cmake_files_headless.sh
@@ -1,6 +1,6 @@
 c_compiler=clang-16
 cxx_compiler=clang++-16
-other_cmake_flags="-DHEADLESS=1"
+other_cmake_flags="-DHEADLESS=1 -DOPENSSL_ROOT_DIR=/usr/local -DOPENSSL_LIBRARIES=/usr/local/lib64"
 
 ${c_compiler} -v && ${cxx_compiler} -v && cmake --version
 export CC=${c_compiler}
