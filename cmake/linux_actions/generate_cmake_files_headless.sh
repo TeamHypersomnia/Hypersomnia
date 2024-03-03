cbe0bcdb14414955541a9dd941876810ac80fd59
geneotech <patryk.czachurski@gmail.com>
2024-03-04 00:11:50 +0100

    Fix Headless build

--- cmake/linux_actions/generate_cmake_files_headless.sh
+++ cmake/linux_actions/generate_cmake_files_headless.sh
@@ -1,6 +1,6 @@
 c_compiler=clang-16
 cxx_compiler=clang++-16
-other_cmake_flags="-DHEADLESS=1 -DOPENSSL_ROOT_DIR=/usr/local -DOPENSSL_LIBRARIES=/usr/local/lib64"
+other_cmake_flags="-DHEADLESS=1 -DOPENSSL_ROOT_DIR=/usr/local -DOPENSSL_LIBRARIES=/usr/local/lib64 -DOPENSSL_USE_STATIC_LIBS=1"
 
 ${c_compiler} -v && ${cxx_compiler} -v && cmake --version
 export CC=${c_compiler}
