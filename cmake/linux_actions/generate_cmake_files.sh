f0576e00612030b680ddd56453c083bd79f85ab8
geneotech <patryk.czachurski@gmail.com>
2024-03-20 21:49:54 +0100

    Use GLFW on Linux

--- cmake/linux_actions/generate_cmake_files.sh
+++ cmake/linux_actions/generate_cmake_files.sh
@@ -1,6 +1,6 @@
 c_compiler=clang-16
 cxx_compiler=clang++-16
-other_cmake_flags="-DLINK_STEAM_INTEGRATION=1"
+other_cmake_flags="-DLINK_STEAM_INTEGRATION=1 -DUSE_GLFW=1"
 
 ${c_compiler} -v && ${cxx_compiler} -v && cmake --version
 export CC=${c_compiler}
