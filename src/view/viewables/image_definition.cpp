41fe10977b6b153dccf221b7fb08585afeef05bb
geneotech <patryk.czachurski@gmail.com>
2024-03-27 23:00:52 +0100

    Fix headless build

--- src/view/viewables/image_definition.cpp
+++ src/view/viewables/image_definition.cpp
@@ -68,6 +68,7 @@ vec2u image_definition_view::read_source_image_size() const {
 	return augs::image::get_size(resolved_source_path);
 }
 
+#if !HEADLESS
 static bool already_packaged(const augs::path_type& source_path) {
 #if PLATFORM_WEB
 	if (::begins_with(source_path.string(), OFFICIAL_CONTENT_DIR.string())) {
@@ -84,7 +85,6 @@ static bool already_packaged(const augs::path_type& source_path) {
 	return false;
 }
 
-#if !HEADLESS
 std::optional<cached_neon_map_in> image_definition_view::should_regenerate_neon_map(
 	const bool force_regenerate
 ) const {
