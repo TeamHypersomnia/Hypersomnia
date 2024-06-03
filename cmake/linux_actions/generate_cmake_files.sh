6c6e494a29a5ef3accd77756d76a44ec0e3897f0
geneotech <patryk.czachurski@gmail.com>
2024-06-03 23:56:15 +0200

    Linux 20.04 build: Fix missing OpenSSL flags

--- cmake/linux_actions/generate_cmake_files.sh
+++ cmake/linux_actions/generate_cmake_files.sh
@@ -1,6 +1,6 @@
 c_compiler=clang-16
 cxx_compiler=clang++-16
-other_cmake_flags="-DLINK_STEAM_INTEGRATION=1 -DUSE_GLFW=1"
+other_cmake_flags="-DLINK_STEAM_INTEGRATION=1 -DUSE_GLFW=1 -DOPENSSL_ROOT_DIR=/usr/local -DOPENSSL_LIBRARIES=/usr/local/lib64 -DOPENSSL_USE_STATIC_LIBS=1"
 
 ${c_compiler} -v && ${cxx_compiler} -v && cmake --version
 export CC=${c_compiler}
