d65157ef9e83bdb9fbaab053e2dd2dc0004a16ff
geneotech <patryk.czachurski@gmail.com>
2025-07-05 18:31:40 +0200

    swap prices of ao44 and covert

--- src/test_scenes/ingredients/guns.cpp
+++ src/test_scenes/ingredients/guns.cpp
@@ -3929,7 +3929,7 @@ namespace test_flavours {
 			set_density_mult(meta, 2.3f);
 			make_default_gun_container(meta, item_holding_stance::PISTOL_LIKE, 1000.f, 0.f, false, "0.1");
 			meta.get<invariants::item>().wield_sound.id = to_sound_id(test_scene_sound_id::STANDARD_PISTOL_DRAW);
-			meta.get<invariants::item>().standard_price = 1000;
+			meta.get<invariants::item>().standard_price = 1100;
 			set_chambering_duration_ms(meta, 200.f);
 			meta.template get<invariants::item>().space_occupied_per_charge = to_space_units("3");
 			only_allow_mag(meta, test_container_items::AO44_MAGAZINE);
@@ -4296,7 +4296,7 @@ namespace test_flavours {
 			set_density_mult(meta, 1.5f);
 			make_default_gun_container(meta, item_holding_stance::PISTOL_LIKE, 1400.f, 0.f, false, "0.01");
 			meta.get<invariants::item>().wield_sound.id = to_sound_id(test_scene_sound_id::STANDARD_PISTOL_DRAW);
-			meta.get<invariants::item>().standard_price = 1100;
+			meta.get<invariants::item>().standard_price = 1000;
 			set_chambering_duration_ms(meta, 500.f);
 			only_allow_mag(meta, test_container_items::COVERT_MAGAZINE);
 			meta.get<invariants::item>().draw_mag_over_when_reloading = false;
